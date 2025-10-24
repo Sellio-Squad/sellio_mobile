6f46ecf29d8febe1b030f885032663b4cac8b9e7
MuhammedWael9991 <mw26200400@gmail.com>
2025-10-25 01:05:05 +0300

    add Sellio theme implementation

---
+++ lib/core/design_system/themes/sellio_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_colors.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_typography.dart';
+
+
+class SellioTheme extends InheritedWidget {
+  final Brightness brightness;
+  final SellioColorScheme colors;
+  final SellioTypography typography;
+
+  const SellioTheme({
+    super.key,
+    required this.brightness,
+    required this.colors,
+    required this.typography,
+    required super.child,
+  });
+
+  static SellioTheme of(BuildContext context) {
+    final result = context.dependOnInheritedWidgetOfExactType<SellioTheme>();
+    assert(result != null, 'No AppTheme found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(SellioTheme oldWidget) {
+    return brightness != oldWidget.brightness ||
+        colors != oldWidget.colors ||
+        typography != oldWidget.typography;
+  }
+}
