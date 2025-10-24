ac5a7ecb7dd53537bd5e5135a109e8679cade250
marah222 <maraharam222@gmail.com>
2025-10-24 14:43:48 +0300

    feat: add Sellio application title

--- lib/ui/main/main.dart
+++ lib/ui/main/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sellio',
+      title: 'Sellio app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
