9652f9ab67b504a7643dd12347f9993ece3894e8
MuhammedWael9991 <mw26200400@gmail.com>
2025-10-26 00:43:20 +0300

    add indicator widget for page navigation

---
+++ lib/core/design_system/widgets/indicator.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
+
+class Indicator extends StatelessWidget {
+  final int pages;
+  final int currentPage;
+
+  const Indicator({
+    super.key,
+    required this.pages,
+    required this.currentPage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.theme.colors;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        pages,
+            (index) {
+          final bool isActive = index == currentPage;
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            margin: const EdgeInsets.symmetric(horizontal: 2),
+            width: isActive ? 6 : 4,
+            height: isActive ? 6 : 4,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: isActive
+                  ? colors.secondary
+                  : colors.stroke,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
