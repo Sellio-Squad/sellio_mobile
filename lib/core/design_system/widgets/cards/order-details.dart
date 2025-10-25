89bb01d18097de29abfec06bfd9dff3607675957
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-10-25 21:47:06 +0300

    reformat code

--- lib/core/design_system/widgets/cards/order-details.dart
+++ lib/core/design_system/widgets/cards/order-details.dart
@@ -4,7 +4,6 @@ import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.da
 
 import '../../constants/assets.dart';
 
-
 enum OrderStatus { processing, completed, cancelled }
 
 class OrderItem {
@@ -79,212 +78,213 @@ class _OrderDetailsState extends State<OrderDetails> {
     }
 
     return Container(
-        padding: const EdgeInsets.all(12),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: context.theme.colors.surface,
-        ),
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-        Row(
-        children: [
-        SvgPicture.asset(Assets.orderIcon, width: 20, height: 20),
-        const SizedBox(width: 4),
-        Text(
-          "Order #${widget.orderId}",
-          style: context.theme.typography.textTheme.labelMedium.copyWith(
-            color: context.theme.colors.title,
-          ),
-        ),
-      const Spacer(),
-      Container(
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: bgColor,
-        ),
-        child: Text(
-          statusText,
-          style: context.theme.typography.textTheme.labelXSmall
-              .copyWith(color: textColor),
-        ),
+      padding: const EdgeInsets.all(12),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: context.theme.colors.surface,
       ),
-      ],
-    ),
-    const SizedBox(height: 8),
-    Align(
-    alignment: Alignment.centerLeft,
-    child: Text(
-    "Placed on ${widget.orderDate}",
-    style: context.theme.typography.textTheme.labelXSmall.copyWith(
-    color: context.theme.colors.body,
-    ),
-    ),
-    ),
-    const SizedBox(height: 12),
-    GestureDetector(
-    onTap: () {
-    setState(() {
-    _isExpanded = !_isExpanded;
-    });
-    },
-    child: Container(
-    padding: const EdgeInsets.symmetric(vertical: 8),
-    child: Row(
-    children: [
-    AnimatedRotation(
-    turns: _isExpanded ? 0.5 : 0,
-    duration: const Duration(milliseconds: 200),
-    child: Icon(
-    Icons.keyboard_arrow_down,
-    color: context.theme.colors.body,
-    size: 20,
-    ),
-    ),
-    const SizedBox(width: 8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset(Assets.orderIcon, width: 20, height: 20),
+              const SizedBox(width: 4),
+              Text(
+                "Order #${widget.orderId}",
+                style: context.theme.typography.textTheme.labelMedium.copyWith(
+                  color: context.theme.colors.title,
+                ),
+              ),
+              const Spacer(),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: bgColor,
+                ),
+                child: Text(
+                  statusText,
+                  style: context.theme.typography.textTheme.labelXSmall
+                      .copyWith(color: textColor),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Placed on ${widget.orderDate}",
+              style: context.theme.typography.textTheme.labelXSmall.copyWith(
+                color: context.theme.colors.body,
+              ),
+            ),
+          ),
+          const SizedBox(height: 12),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _isExpanded = !_isExpanded;
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Row(
+                children: [
+                  AnimatedRotation(
+                    turns: _isExpanded ? 0.5 : 0,
+                    duration: const Duration(milliseconds: 200),
+                    child: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: context.theme.colors.body,
+                      size: 20,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
 
-    CircleAvatar(
-    radius: 28,
-    backgroundImage: AssetImage(widget.marketImage),
-    ),
-    const SizedBox(width: 8),
+                  CircleAvatar(
+                    radius: 28,
+                    backgroundImage: AssetImage(widget.marketImage),
+                  ),
+                  const SizedBox(width: 8),
 
-    Expanded(
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    Text(
-    widget.marketName,
-    style: context.theme.typography.textTheme.labelSmall
-        .copyWith(color: context.theme.colors.body),
-    ),
-    const SizedBox(height: 2),
-    Text(
-    "${widget.orderTotal} items",
-    style: context.theme.typography.textTheme.labelXSmall
-        .copyWith(color: context.theme.colors.body),
-    ),
-    ],
-    ),
-    ),
-    ],
-    ),
-    ),
-    ),
-    AnimatedCrossFade(
-    firstChild: const SizedBox.shrink(),
-    secondChild: Container(
-    margin: const EdgeInsets.only(top: 8),
-    child: Column(
-    children: widget.orderItems.map((item) {
-    return Container(
-    padding: const EdgeInsets.fromLTRB(24, 5, 12, 5),
-    child: Row(
-    children: [
-    Text(
-    "${item.quantity}X",
-    style: context.theme.typography.textTheme.labelSmall
-        .copyWith(color: context.theme.colors.body),
-    ),
-    const SizedBox(width: 8),
-    Expanded(
-    child: Text(
-    item.productName,
-    style: context.theme.typography.textTheme.labelSmall
-        .copyWith(color: context.theme.colors.body),
-    ),
-    ),
-    Text(
-    "\$${item.price.toStringAsFixed(2)}",
-    style: context.theme.typography.textTheme.labelSmall
-        .copyWith(color: context.theme.colors.primary),
-    ),
-    ],
-    ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.marketName,
+                          style: context.theme.typography.textTheme.labelSmall
+                              .copyWith(color: context.theme.colors.body),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          "${widget.orderTotal} items",
+                          style: context.theme.typography.textTheme.labelXSmall
+                              .copyWith(color: context.theme.colors.body),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          AnimatedCrossFade(
+            firstChild: const SizedBox.shrink(),
+            secondChild: Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Column(
+                children: widget.orderItems.map((item) {
+                  return Container(
+                    padding: const EdgeInsets.fromLTRB(24, 5, 12, 5),
+                    child: Row(
+                      children: [
+                        Text(
+                          "${item.quantity}X",
+                          style: context.theme.typography.textTheme.labelSmall
+                              .copyWith(color: context.theme.colors.body),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: Text(
+                            item.productName,
+                            style: context.theme.typography.textTheme.labelSmall
+                                .copyWith(color: context.theme.colors.body),
+                          ),
+                        ),
+                        Text(
+                          "\$${item.price.toStringAsFixed(2)}",
+                          style: context.theme.typography.textTheme.labelSmall
+                              .copyWith(color: context.theme.colors.primary),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            crossFadeState: _isExpanded
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
+            duration: const Duration(milliseconds: 200),
+          ),
+
+          const SizedBox(height: 16),
+          if (widget.status == OrderStatus.processing)
+            Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: context.theme.colors.primary,
+                      foregroundColor: context.theme.colors.onPrimary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                    ),
+                    onPressed: widget.onViewDetailsClick,
+                    child: Text(
+                      "View details",
+                      style: context.theme.typography.textTheme.labelSmall,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: context.theme.colors.errorVariant,
+                      foregroundColor: context.theme.colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                    ),
+                    onPressed: widget.onCancelClick,
+                    child: Text(
+                      "Cancel Order",
+                      style: context.theme.typography.textTheme.labelSmall,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          else
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: context.theme.colors.primary,
+                  foregroundColor: context.theme.colors.onPrimary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 12,
+                  ),
+                ),
+                onPressed: widget.onOrderAgainClick,
+                child: Text(
+                  "Order again",
+                  style: context.theme.typography.textTheme.labelSmall,
+                ),
+              ),
+            ),
+        ],
+      ),
     );
-    }).toList(),
-    ),
-    ),
-    crossFadeState: _isExpanded
-    ? CrossFadeState.showSecond
-        : CrossFadeState.showFirst,
-    duration: const Duration(milliseconds: 200),
-    ),
-
-    const SizedBox(height: 16),
-    if (widget.status == OrderStatus.processing)
-    Row(
-    children: [
-    Expanded(
-    flex: 3,
-    child: ElevatedButton(
-    style: ElevatedButton.styleFrom(
-    backgroundColor: context.theme.colors.primary,
-    foregroundColor: context.theme.colors.onPrimary,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(8),
-    ),
-    padding: const EdgeInsets.symmetric(
-    horizontal: 24,
-    vertical: 12,
-    ),
-    ),
-    onPressed: widget.onViewDetailsClick,
-    child: Text(
-    "View details",
-    style: context.theme.typography.textTheme.labelSmall,
-    ),
-    ),
-    ),
-    const SizedBox(width: 8),
-    Expanded(
-    flex: 2,
-    child: ElevatedButton(
-    style: ElevatedButton.styleFrom(
-    backgroundColor: context.theme.colors.errorVariant,
-    foregroundColor: context.theme.colors.red,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(8),
-    ),
-    padding: const EdgeInsets.symmetric(
-    horizontal: 24,
-    vertical: 12,
-    ),
-    ),
-    onPressed: widget.onCancelClick,
-    child: Text(
-    "Cancel Order",
-    style: context.theme.typography.textTheme.labelSmall,
-    ),
-    ),
-    ),
-    ],
-    )
-    else
-    SizedBox(
-    width: double.infinity,
-    child: ElevatedButton(
-    style: ElevatedButton.styleFrom(
-    backgroundColor: context.theme.colors.primary,
-    foregroundColor: context.theme.colors.onPrimary,
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(8),
-    ),
-    padding: const EdgeInsets.symmetric(
-    horizontal: 24,
-    vertical: 12,
-    ),
-    ),
-    onPressed: widget.onOrderAgainClick,
-    child: Text(
-    "Order again",
-    style: context.theme.typography.textTheme.labelSmall,
-    ),
-    ),
-    ),
-
-
   }
 }
