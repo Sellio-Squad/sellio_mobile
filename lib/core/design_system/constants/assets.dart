4917ccccfc85337cd90507b0354515d6c484a1ba
Arzo-zi <zinahashraf88@gmail.com>
2025-10-25 17:02:50 +0300

    feat: add user icon

--- lib/core/design_system/constants/assets.dart
+++ lib/core/design_system/constants/assets.dart
@@ -9,4 +9,5 @@ class Assets {
   static const String alertDiamond = '$iconsPath/alert-diamond.svg';
   static const String checkmarkBadge = '$iconsPath/checkmark-badge-02.svg';
   static const String cancelCircle = '$iconsPath/cancel-circle.svg';
+  static const String user= '$iconsPath/user.svg';
 }
