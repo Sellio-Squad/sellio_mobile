78de995db99786f8ec190d908749bf4b7adb235e
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-10-26 02:17:18 +0300

    feat: refactor chip widget to ChipCategory

--- lib/core/design_system/widgets/ChipCategory.dart
+++ lib/core/design_system/widgets/ChipCategory.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
 
-class Chip extends StatelessWidget {
+class ChipCategory extends StatelessWidget {
   final String label;
   final IconData? icon;
   final bool selected;
   final VoidCallback onTap;
   final ShapeBorder shape;
 
-  const Chip({
+  const ChipCategory({
     super.key,
     required this.label,
     this.icon,
     required this.selected,
     required this.onTap,
     this.shape = const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(16)),
+      borderRadius: BorderRadius.all(Radius.circular(100)),
     ),
   });
 
   @override
   Widget build(BuildContext context) {
     final backgroundColor = selected ? context.theme.colors.primary  : context.theme.colors.surfaceLow;
-    final textColor = selected ? context.theme.colors.onPrimary : context.theme.colors.body;
+    final contentColor = selected ? context.theme.colors.onPrimary : context.theme.colors.body;
 
     return Material(
       color: backgroundColor,
@@ -31,25 +31,25 @@ class Chip extends StatelessWidget {
         onTap: onTap,
         customBorder: shape,
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 11),
+          padding: const EdgeInsets.fromLTRB(4, 5, 16, 5),
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               if (icon != null) ...[
                 Container(
-                  width: 28,
-                  height: 28,
+                  width: 30,
+                  height: 30,
                   decoration: const BoxDecoration(
                     color: Color(0x1FFFFFFF),
                     shape: BoxShape.circle,
                   ),
-                  child: Icon(icon??Icons.abc, size: 20, color: textColor),
+                  child: Icon(icon, size: 20, color: contentColor),
                 ),
                 const SizedBox(width: 8),
               ],
               Text(
                 label,
-                style: TextStyle(color: textColor),
+                style: TextStyle(color: contentColor),
               ),
             ],
           ),
