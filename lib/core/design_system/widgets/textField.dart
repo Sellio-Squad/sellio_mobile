b219ef9b597813f1a8d8f8dd171ca135029c4b63
Arzo-zi <zinahashraf88@gmail.com>
2025-10-27 01:32:05 +0300

    refactor: TextField

--- lib/core/design_system/widgets/textField.dart
+++ lib/core/design_system/widgets/textField.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sellio_mobile/core/design_system/constants/assets.dart';
@@ -6,12 +8,44 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class SellioTextField extends StatefulWidget {
   final bool isParagraph;
-  final bool showEye;
+  final TextInputType? inputType;
+  final BorderRadiusGeometry cornerRadius;
+  final Color shadowColor;
+  final TextStyle? textStyle;
+  final int? maxLine;
+  final bool isTextFieldFilled;
+  final Color? fillColor;
+  final String hintText;
+  final TextStyle? hintStyle;
+  final EdgeInsetsGeometry prefixIconPadding;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final double enabledBorderRadius;
+  final double focusedBorderRadius;
+  final double errorBorderRadius;
+  final double focusedErrorBorderRadius;
+  final TextStyle? errorStyle;
 
   const SellioTextField({
     super.key,
     this.isParagraph = false,
-    this.showEye = true,
+    this.inputType,
+    this.cornerRadius = const BorderRadius.all(Radius.circular(8)),
+    this.shadowColor = const Color(0x1F520826),
+    this.textStyle,
+    this.maxLine,
+    this.isTextFieldFilled = true,
+    this.fillColor,
+    this.hintText = 'Full name',
+    this.hintStyle,
+    this.prefixIconPadding = const EdgeInsets.only(left: 16, right: 12),
+    this.prefixIcon,
+    this.suffixIcon,
+    this.enabledBorderRadius = 8.0,
+    this.focusedBorderRadius = 8.0,
+    this.errorBorderRadius = 8.0,
+    this.focusedErrorBorderRadius = 8.0,
+    this.errorStyle,
   });
 
   @override
@@ -49,7 +83,7 @@ class _SellioTextFieldState extends State<SellioTextField> {
         : context.theme.colors.stroke;
 
     final Color iconColor = isError
-        ? Colors.red
+        ? context.theme.colors.neutralsHint
         : isFocused
         ? context.theme.colors.primary
         : context.theme.colors.body;
@@ -58,20 +92,53 @@ class _SellioTextFieldState extends State<SellioTextField> {
         ? context.theme.colors.title
         : context.theme.colors.body;
 
+    final List<BoxShadow> textFieldShadow = isFocused && !isError
+        ? [
+            BoxShadow(
+              color: widget.shadowColor,
+              blurRadius: 8,
+              offset: Offset(0, 4),
+            ),
+          ]
+        : [];
+
+    final textFieldStyle =
+        widget.textStyle ??
+        context.theme.typography.textTheme.bodyMedium.copyWith(
+          color: isFocused
+              ? context.theme.colors.title
+              : context.theme.colors.body,
+        );
+
+    final maxline = widget.isParagraph
+        ? (widget.maxLine ?? null)
+        : (widget.maxLine ?? 1);
+
+    final filledColor = widget.fillColor ?? context.theme.colors.surface;
+
+    final hintTextStyle =
+        widget.hintStyle ??
+        context.theme.typography.textTheme.labelMedium.copyWith(
+          color: hintColor,
+        );
+
+    final String? errorText = widget.isParagraph
+        ? null
+        : (isError ? 'Error message!' : null);
+
+    final errorStyle =
+        widget.errorStyle ?? context.theme.typography.textTheme.labelSmall.copyWith(
+          color: context.theme.colors.semanticError,
+        );
+
+
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: isFocused && !isError
-            ? [
-                const BoxShadow(
-                  color: Color(0x1F520826),
-                  blurRadius: 8,
-                  offset: Offset(0, 4),
-                ),
-              ]
-            : [],
+        borderRadius: widget.cornerRadius,
+        boxShadow: textFieldShadow,
       ),
       child: TextField(
+        keyboardType: widget.inputType ?? TextInputType.text,
         focusNode: _focusNode,
         controller: _controller,
         onChanged: (value) {
@@ -80,95 +147,81 @@ class _SellioTextFieldState extends State<SellioTextField> {
           });
         },
         obscureText: isObscured,
-        onEditingComplete: () {
-          _focusNode.unfocus();
-          setState(() {});
-        },
-        style: context.theme.typography.textTheme.bodyMedium.copyWith(
-          color: isFocused
-              ? context.theme.colors.title
-              : context.theme.colors.body,
-        ),
-        maxLines: widget.isParagraph ? null : 1,
-
+        style: textFieldStyle,
+        maxLines: maxline,
         decoration: InputDecoration(
-          filled: true,
-          fillColor: context.theme.colors.surface,
-          hintText: 'Full name',
-          hintStyle: context.theme.typography.textTheme.labelMedium.copyWith(
-            color: hintColor,
-          ),
+          filled: widget.isTextFieldFilled,
+          fillColor: filledColor,
+          hintText: widget.hintText,
+          hintStyle: hintTextStyle,
           prefixIcon: widget.isParagraph
               ? null
-              : Padding(
-                  padding: const EdgeInsets.only(left: 16, right: 12),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SvgPicture.asset(
-                        Assets.user,
-                        colorFilter: ColorFilter.mode(
-                          iconColor,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      if (isError) ...[
-                        const SizedBox(width: 12),
-                        Container(
-                          width: 1,
-                          height: 30,
-                          color: context.theme.colors.stroke,
-                        ),
-                      ],
-                    ],
-                  ),
-                ),
+              : widget.prefixIcon ?? _buildPrefixIcon(iconColor),
           prefixIconConstraints: const BoxConstraints(
             minWidth: 24,
             minHeight: 24,
           ),
-
-          suffixIcon: widget.isParagraph || !widget.showEye
-              ? null
-              : IconButton(
-                  icon: SvgPicture.asset(
-                    isObscured ? Assets.closeEye : Assets.openEye,
-                    colorFilter: ColorFilter.mode(
-                      context.theme.colors.body,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      isObscured = !isObscured;
-                    });
-                  },
-                ),
-
+          suffixIcon: _buildSuffixIcon(iconColor),
           enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(widget.enabledBorderRadius),
             borderSide: BorderSide(color: borderColor, width: 0.5),
           ),
           focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(widget.focusedBorderRadius),
             borderSide: BorderSide(color: borderColor),
           ),
           errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(color: Colors.red),
+            borderRadius: BorderRadius.circular(widget.errorBorderRadius),
+            borderSide: BorderSide(color: context.theme.colors.semanticError),
           ),
           focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(color: Colors.red),
+            borderRadius: BorderRadius.circular(
+              widget.focusedErrorBorderRadius,
+            ),
+            borderSide: BorderSide(color: context.theme.colors.semanticError),
           ),
-          errorText: widget.isParagraph
-              ? null
-              : isError
-              ? 'Error message!'
-              : null,
-          errorStyle: context.theme.typography.textTheme.labelSmall,
+          errorText: errorText,
+          errorStyle: errorStyle,
         ),
       ),
+    );
+  }
+
+  Widget? _buildPrefixIcon(Color iconColor) {
+    if (widget.isParagraph) return null;
+
+    return Padding(
+      padding: widget.prefixIconPadding,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            Assets.user,
+            colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+          ),
+          if (isError) ...[
+            const SizedBox(width: 12),
+            Container(width: 1, height: 30, color: context.theme.colors.stroke),
+          ],
+        ],
+      ),
+    );
+  }
+  Widget? _buildSuffixIcon(Color iconColor) {
+    if (widget.isParagraph) return null;
+
+    if (widget.suffixIcon != null) return widget.suffixIcon;
+
+    return IconButton(
+      icon: SvgPicture.asset(
+        isObscured ? Assets.closeEye : Assets.openEye,
+        colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+      ),
+      onPressed: () {
+        setState(() {
+          isObscured = !isObscured;
+        });
+      },
     );
   }
 }
