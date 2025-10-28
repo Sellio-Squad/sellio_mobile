c8dd3e5e65f0a6fcd9d129f6cc985b091f0d99fe
Arzo-zi <zinahashraf88@gmail.com>
2025-10-29 01:43:38 +0300

    refactor: login screen

--- lib/core/design_system/constants/app_strings.dart
+++ lib/core/design_system/constants/app_strings.dart
@@ -6,4 +6,13 @@ class AppStrings {
   static const String cart = 'Cart';
   static const String thrift = 'Thrift';
   static const String account = 'Account';
+
+  // login screen
+  static const String titleLogin = 'Welcome Back!';
+  static const String subtitleLogin ='Enter your information to login';
+  static const String phoneNumber = 'Phone number';
+  static const String login = 'Login';
+  static const String or = 'OR';
+  static const String createAccount = 'Create Account';
+  static const String continueAsGuest = 'Continue as guest';
 }
