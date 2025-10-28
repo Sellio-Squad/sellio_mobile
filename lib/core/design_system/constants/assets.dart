b88cc452b171a141c73d1cd6cf7ef264fb9876cd
Arzo-zi <zinahashraf88@gmail.com>
2025-10-28 19:53:53 +0300

    refactor: remove duplicate asset constants

--- lib/core/design_system/constants/assets.dart
+++ lib/core/design_system/constants/assets.dart
@@ -22,26 +22,10 @@ class Assets {
   static const String favouriteIcon = '$iconsPath/favourite_icon.svg';
   static const String discountIcon = '$iconsPath/discount_icon.svg';
   static const String discountFrame = '$iconsPath/discount_frame.svg';
-
-
   //login
   static const String sellioWhite = '$imagesPath/sellio_white.png';
   static const String stars = '$iconsPath/stars.svg';
   static const String loginTopSection = '$imagesPath/login_top_section.png';
-
-
-
-
-
-  //login
-  static const String sellioWhite = '$imagesPath/sellio_white.png';
-  static const String stars = '$iconsPath/stars.svg';
-  static const String loginTopSection = '$imagesPath/login_top_section.png';
-
-
-
-
-
   //images
   static const String cakeHouseLogo = '$imagesPath/cake-house.png';
   // Product Card Icons
@@ -51,13 +35,11 @@ class Assets {
   static const String cart = '$iconsPath/small_cart.svg';
   static const String unselectedFavorite =
       '$iconsPath/unselected-favourite.svg';
-
   // Category Icons
   static const String allCategories = '$iconsPath/all.svg';
   static const String food = '$iconsPath/food.svg';
   static const String drinks = '$iconsPath/drinks.svg';
   static const String clothes = '$iconsPath/clothes.svg';
-
   // text field icons
   static const String user= '$iconsPath/user.svg';
   static const String openEye = '$iconsPath/opened_eye.svg';
@@ -66,7 +48,4 @@ class Assets {
   static const String phone = '$iconsPath/phone.svg';
   static const String arrowDown = '$iconsPath/arrow_down.svg';
   static const String password = '$iconsPath/password.svg';
-
-
-
 }
