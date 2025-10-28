9a4047d628438ac26b8310b0ca6277e025bdae7f
MuhammedWael9991 <mw26200400@gmail.com>
2025-10-28 23:46:26 +0300

    add image layout and note input

--- lib/ui/screens/ProductDetailsScreen.dart
+++ lib/ui/screens/ProductDetailsScreen.dart
@@ -2,7 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sellio_mobile/core/design_system/constants/assets.dart';
 import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
+import 'package:sellio_mobile/core/design_system/widgets/buttons/button.dart';
 import 'package:sellio_mobile/core/design_system/widgets/sellio_app_bar.dart';
+import 'package:sellio_mobile/core/design_system/widgets/textField.dart';
+
+void main() {
+  runApp(
+    SellioThemeProvider(brightness: Brightness.light, child: MyPreviewApp()),
+  );
+}
+
+class MyPreviewApp extends StatelessWidget {
+  const MyPreviewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const ProductDetailsScreen(),
+    );
+  }
+}
 
 class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({super.key});
@@ -48,88 +68,107 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           padding: EdgeInsets.zero,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: 110,
-                    height: 110,
-                    color: Colors.grey[200],
-                    child: Image.asset(
-                      'assets/images/lemon_popsicle.jpg',
-                      width: 110,
-                      height: 110,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Center(
-                          child: Icon(
-                            Icons.image_outlined,
-                            size: 40,
-                            color: Colors.grey[400],
-                          ),
-                        );
-                      },
+                Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Container(
+                        width: 110,
+                        height: 110,
+                        color: Colors.grey[200],
+                        child: Image.asset(
+                          'assets/images/lemon_popsicle.jpg',
+                          width: 110,
+                          height: 110,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Center(
+                              child: Icon(
+                                Icons.image_outlined,
+                                size: 40,
+                                color: Colors.grey[400],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 4),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Container(
+                        width: 110,
+                        height: 110,
+                        color: Colors.grey[200],
+                        child: Image.asset(
+                          'assets/images/lemon_cheesecake.jpg',
+                          width: 110,
+                          height: 110,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Center(
+                              child: Icon(
+                                Icons.image_outlined,
+                                size: 40,
+                                color: Colors.grey[400],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 4),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: 110,
-                    height: 110,
-                    color: Colors.grey[200],
-                    child: Image.asset(
-                      'assets/images/lemon_cheesecake.jpg',
-                      width: 110,
-                      height: 110,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Center(
-                          child: Icon(
-                            Icons.image_outlined,
-                            size: 40,
-                            color: Colors.grey[400],
-                          ),
-                        );
-                      },
+                const SizedBox(width: 4),
+                Expanded(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      height: 224,
+                      color: Colors.grey[200],
+                      child: Image.asset(
+                        'assets/images/lemon_cake_main.jpg',
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Center(
+                            child: Icon(
+                              Icons.image_outlined,
+                              size: 60,
+                              color: Colors.grey[400],
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
-            const SizedBox(width: 4),
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  height: 224,
-                  color: Colors.grey[200],
-                  child: Image.asset(
-                    'assets/images/lemon_cake_main.jpg',
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Center(
-                        child: Icon(
-                          Icons.image_outlined,
-                          size: 60,
-                          color: Colors.grey[400],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: SellioTextField(
+              isParagraph: true,
+              hintText: "Note  (Optional)",
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SellioButton(
+          text: "Add to cart",
+          onTap: () {},
+          suffixSvgPath: Assets.cart,
+        ),
+      )
     );
   }
 }
