7a219a7f5d8ec41a189ba480350d94e8c5aa9e8b
yaseenOmar <yz456359@gmail.com>
2025-10-29 14:36:15 +0300

    Add app bar to CustomizeYourProductScreen

--- lib/ui/screens/CustomizeYourProductScreen.dart
+++ lib/ui/screens/CustomizeYourProductScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
+import 'package:sellio_mobile/core/design_system/widgets/sellio_app_bar.dart';
+
+import '../../core/design_system/constants/assets.dart';
 
 class CustomizeYourProductScreen extends StatefulWidget {
   const CustomizeYourProductScreen({super.key});
@@ -9,17 +12,16 @@ class CustomizeYourProductScreen extends StatefulWidget {
       _CustomizeYourProductScreenState();
 }
 
-class _CustomizeYourProductScreenState
-    extends State<CustomizeYourProductScreen> {
+class _CustomizeYourProductScreenState extends State<CustomizeYourProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Custom Yuor Product Screen',
-        style: context.theme.typography.textTheme.headlineLarge.copyWith(
-          color: context.theme.colors.primary,
-        ),
-      ),
+    return Scaffold(
+      appBar: SellioAppBar(
+        title: "Customize Your Product ",
+        showLeading: false,
+        subtitle: "Choose a product to customize it ",
+        actionIcon: Assets.orderIcon,
+      )
     );
   }
 }
