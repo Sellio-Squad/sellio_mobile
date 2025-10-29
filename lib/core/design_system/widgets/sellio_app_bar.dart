6792b125e3b8526f5ee0cf7e8a4a6ef81fa6411f
yaseenOmar <yz456359@gmail.com>
2025-10-29 14:24:03 +0300

    Add subtitle support to app bar

--- lib/core/design_system/widgets/sellio_app_bar.dart
+++ lib/core/design_system/widgets/sellio_app_bar.dart
@@ -14,6 +14,7 @@ class SellioAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   final bool showNotificationIcon;
   final bool showLeading;
+  final String? subtitle;
 
   const SellioAppBar({
     super.key,
@@ -26,8 +27,8 @@ class SellioAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.backgroundColor,
     this.showNotificationIcon = true,
     this.showLeading = true,
+    this.subtitle,
   });
-
 
   @override
   Widget build(BuildContext context) {
@@ -38,66 +39,83 @@ class SellioAppBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       flexibleSpace: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          padding: const EdgeInsets.only(left: 16, right: 16 , bottom: 12,top: 8),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              if(showLeading)
-              if (showBack)
-                IconButton(
-                  icon: SvgPicture.asset(Assets.arrowLeft),
-                  onPressed: () => Navigator.of(context).pop(),
-                )
-              else
-                Image.asset(
-                  Assets.sellio,
-                  height: 58,
-                  width: 61,
-                ),
+              if (showLeading)
+                if (showBack)
+                  IconButton(
+                    icon: SvgPicture.asset(Assets.arrowLeft),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )
+                else
+                  Image.asset(Assets.sellio, height: 58, width: 61),
               Expanded(
                 child: showGreeting
                     ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Welcome, $userName',
-                      style: context.theme.typography.textTheme.labelSmall.copyWith(
-                        color: context.theme.colors.title,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          Assets.location,
-                          width: 20,
-                          height: 20,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          location ?? '',
-                          style: context.theme.typography.textTheme.labelXSmall.copyWith(
-                            color: context.theme.colors.body,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Welcome, $userName',
+                            style: context.theme.typography.textTheme.labelSmall
+                                .copyWith(color: context.theme.colors.title),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-                    : Text(
-                  title ?? '',
-                  style: context.theme.typography.textTheme.titleMedium.copyWith(
-                    color: context.theme.colors.title,
-                  ),
-                  textAlign: TextAlign.start,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                Assets.location,
+                                width: 20,
+                                height: 20,
+                              ),
+                              const SizedBox(width: 4),
+                              Text(
+                                location ?? '',
+                                style: context
+                                    .theme
+                                    .typography
+                                    .textTheme
+                                    .labelXSmall
+                                    .copyWith(color: context.theme.colors.body),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            title ?? '',
+                            style: context
+                                .theme
+                                .typography
+                                .textTheme
+                                .titleMedium
+                                .copyWith(color: context.theme.colors.title),
+                            textAlign: TextAlign.start,
+                          ),
+
+                          Text(
+                            subtitle ?? '',
+                            style: context
+                                .theme
+                                .typography
+                                .textTheme
+                                .labelSmall
+                                .copyWith(color: context.theme.colors.body),
+                            textAlign: TextAlign.start,
+                          ),
+                        ],
+                      ),
+              ),
+              if (showNotificationIcon)
+                IconButton(
+                  icon: SvgPicture.asset(Assets.bell),
+                  onPressed: onNotificationTap,
                 ),
-              ),
-              if(showNotificationIcon)
-              IconButton(
-                icon: SvgPicture.asset(Assets.bell),
-                onPressed: onNotificationTap,
-              ),
             ],
           ),
         ),
