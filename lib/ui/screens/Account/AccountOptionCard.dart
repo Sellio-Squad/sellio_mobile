f89f6c0ed5b5112bd8b67f77857d8a4489017601
Arzo-zi <zinahashraf88@gmail.com>
2025-11-02 03:24:13 +0300

    feat: implement account screen UI

---
+++ lib/ui/screens/Account/AccountOptionCard.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gap/flutter_gap.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
+
+class AccountOptionCard extends StatelessWidget {
+  final Color? cardColor;
+  final double borderRadius;
+  final String prefixIcon;
+  final double iconSize;
+  final Widget? trailing;
+  final String orderTitle;
+  final VoidCallback onCardClicked;
+
+  const AccountOptionCard({
+    super.key,
+    this.cardColor,
+    this.borderRadius = 8,
+    required this.prefixIcon ,
+    this.trailing,
+    this.iconSize = 24,
+    required this.orderTitle,
+    required this.onCardClicked,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onCardClicked,
+      child: Container(
+        decoration: BoxDecoration(
+          color: cardColor ?? context.theme.colors.surface,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                prefixIcon,
+                width: iconSize,
+                height: iconSize,
+              ),
+              Gap(8),
+              Expanded(
+                child: Text(
+                  orderTitle,
+                  style: context.theme.typography.textTheme.labelMedium.copyWith(
+                    color: context.theme.colors.title,
+                  ),
+                ),
+              ),
+              if (trailing != null) trailing!,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/Account/account_screen.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gap/flutter_gap.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_colors.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_typography.dart';
+import 'package:sellio_mobile/core/design_system/widgets/buttons/switch.dart';
+import 'package:sellio_mobile/core/design_system/widgets/sellio_app_bar.dart';
+import 'package:sellio_mobile/ui/screens/Account/AccountOptionCard.dart';
+import '../../../core/design_system/constants/assets.dart';
+import 'package:flutter/services.dart';
+
+class AccountScreen extends StatefulWidget {
+  const AccountScreen({super.key});
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    SellioTextTheme themeText = context.theme.typography.textTheme;
+    SellioColorScheme colors = context.theme.colors;
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark.copyWith(
+        statusBarColor: colors.surfaceLow,
+      ),
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: colors.surfaceLow,
+        appBar: SellioAppBar(
+          title: 'Account',
+          showBack: true,
+          showLeading: false,
+          trailing: SvgPicture.asset(Assets.moreHorizontalSquare,
+              width: 24, height: 24),
+        ),
+        body: SafeArea(
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        uploadImageCard(
+                          imagePath: Assets.cat,
+                          editIconPath: Assets.pencilEdit,
+                          context: context,
+                          onEditTap: () {},
+                        ),
+                        Gap(12),
+                        Text(
+                          'Zinah & Baraa',
+                          style: themeText.titleSmall
+                              .copyWith(color: colors.title),
+                        ),
+                        Text(
+                          'Hamsa2025@gmail.com',
+                          style:
+                              themeText.labelSmall.copyWith(color: colors.body),
+                        )
+                      ],
+                    ),
+                  ),
+                  Gap(24),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: AccountCustomCard(
+                            icon: Assets.package, orderTitle: 'My Orders'),
+                      ),
+                      Gap(8),
+                      Expanded(
+                        child: AccountCustomCard(
+                            icon: Assets.heartCheck,
+                            orderTitle: 'My Favourites'),
+                      ),
+                    ],
+                  ),
+                  Gap(16),
+                  AccountOptionCard(
+                    prefixIcon: Assets.repair,
+                    orderTitle: 'Account Settings',
+                    onCardClicked: () {},
+                    trailing: SvgPicture.asset(Assets.arrowRightCustom),
+                  ),
+                  Gap(12),
+                  AccountOptionCard(
+                    prefixIcon: Assets.circleLockAdd,
+                    orderTitle: 'Reset Password',
+                    onCardClicked: () {},
+                    trailing: SvgPicture.asset(Assets.arrowRightCustom),
+                  ),
+                  Gap(12),
+                  AccountOptionCard(
+                    prefixIcon: Assets.languageCircle,
+                    orderTitle: 'Language',
+                    onCardClicked: () {},
+                    trailing: SvgPicture.asset(Assets.arrowRightCustom),
+                  ),
+                  Gap(12),
+                  AccountOptionCard(
+                    prefixIcon: Assets.notification,
+                    orderTitle: 'Notifications',
+                    onCardClicked: () {},
+                    trailing: DesignSwitch(
+                      value: true,
+                      onChanged: (bool value) {},
+                    ),
+                  ),
+                  Gap(12),
+                  AccountOptionCard(
+                    prefixIcon: Assets.mobileProgramming,
+                    orderTitle: 'App version',
+                    onCardClicked: () {},
+                    trailing: Text(
+                      '1.2',
+                      style: themeText.labelSmall.copyWith(
+                        color: colors.body,
+                      ),
+                    ),
+                  ),
+                  Gap(12),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget uploadImageCard({
+  required String imagePath,
+  required String editIconPath,
+  required VoidCallback onEditTap,
+  required BuildContext context,
+  double shadowBlur = 8,
+}) {
+  return SizedBox(
+    width: 140,
+    height: 140,
+    child: Stack(
+      alignment: Alignment.center,
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: 136,
+          height: 136,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, 4),
+                color: const Color(0x1F000000),
+                blurRadius: shadowBlur,
+              ),
+            ],
+          ),
+        ),
+        ClipOval(
+          child: Container(
+            width: 136,
+            height: 136,
+            child: Stack(
+              clipBehavior: Clip.hardEdge,
+              children: [
+                CircleAvatar(
+                  radius: 68,
+                  backgroundImage: AssetImage(imagePath),
+                  backgroundColor: context.theme.colors.surface,
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 2,
+                  right: 2,
+                  child: GestureDetector(
+                    onTap: onEditTap,
+                    child: Container(
+                      width: 118,
+                      height: 32,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          const BoxShadow(
+                            color: Color(0x70000000),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          editIconPath,
+                          width: 20,
+                          height: 20,
+                          colorFilter: ColorFilter.mode(
+                            context.theme.colors.onPrimary,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+class AccountCustomCard extends StatelessWidget {
+  final Color? cardColor;
+  final double borderRadius;
+  final String icon;
+  final double iconSize;
+  final String orderTitle;
+
+  const AccountCustomCard({
+    super.key,
+    this.cardColor,
+    this.borderRadius = 8,
+    required this.icon,
+    this.iconSize = 28,
+    required this.orderTitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      width: 160,
+      decoration: BoxDecoration(
+        color: cardColor ?? context.theme.colors.surface,
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gap(12),
+            SvgPicture.asset(
+              icon,
+              width: iconSize,
+              height: iconSize,
+            ),
+            Gap(16),
+            Text(
+              orderTitle,
+              style: context.theme.typography.textTheme.labelMedium.copyWith(
+                color: context.theme.colors.title,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
