9fd1b4a9d567619098fdafcd142e24886805ed93
Arzo-zi <zinahashraf88@gmail.com>
2025-11-04 02:05:06 +0300

    feat: add base bottom sheet

---
+++ lib/core/design_system/widgets/sellio_bottom_sheet.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
+
+class SellioBottomSheet extends StatelessWidget {
+  final Widget content;
+  final double? height;
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  SellioBottomSheet(
+      {super.key,
+      required this.content,
+      this.height ,});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        if (didPop) {
+          return;
+        }
+        final NavigatorState? childNavigator = navigatorKey.currentState;
+        if (childNavigator != null && childNavigator.canPop()) {
+          childNavigator.pop();
+        } else {
+          Navigator.of(context).pop();
+        }
+      },
+      child: SizedBox(
+        height: height?? MediaQuery.of(context).size.height *0.25,
+        child: Column(
+          children: [
+            _CustomDragHandle(),
+            Expanded(
+              child: Navigator(
+                key: navigatorKey,
+                onGenerateRoute: (settings) {
+                  return MaterialPageRoute(
+                    builder: (context) {
+                      return content;
+                    },
+                    // => nextBottomSheet ?? Container(),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomDragHandle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 6, bottom: 22),
+        child: Container(
+          width: 44,
+          height: 4,
+          decoration: BoxDecoration(
+            color: context.theme.colors.stroke,
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+      ),
+    );
+  }
+}
