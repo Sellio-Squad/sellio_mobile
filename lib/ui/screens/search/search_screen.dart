44f4a137d2cd175ebd0dc50f8c4e9cf665ed2620
yaseenOmar <yz456359@gmail.com>
2025-11-06 16:38:36 +0300

    Implement search state logic and display results

--- lib/ui/screens/search/search_screen.dart
+++ lib/ui/screens/search/search_screen.dart
@@ -7,6 +7,7 @@ import 'package:sellio_mobile/core/design_system/widgets/sellio_app_bar.dart';
 import 'package:sellio_mobile/ui/screens/home/widgets/search_bar/search_widget.dart';
 
 import '../../../core/design_system/constants/assets.dart';
+import '../../../core/design_system/widgets/cards/product_vertical_card.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -16,9 +17,11 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  String _searchText = '';
   String _selectedCategory = 'Products';
 
-  final List<String> recentSearches = [
+  final List<String> recentSearches =
+  [
     'Cake',
     'Donut',
     'Cola Diet',
@@ -49,20 +52,40 @@ class _SearchScreenState extends State<SearchScreen> {
           horizontal: 16,
         ),
         child: SearchBarWithFilter(
-            onFilterIconClicked: () {}, onTextSubmitted: (String text) {}),
+            onFilterIconClicked: () {}, onTextSubmitted: (String text) {
+              setState(() {
+                _searchText = text;
+              });
+        }),
       ),
     );
   }
 
   SliverToBoxAdapter _buildSearchBodySection(BuildContext context) {
-    SearchState _state = SearchState.noResult;
+    late final SearchState state;
+
+    if (_searchText.isEmpty) {
+      if (recentSearches.isEmpty) {
+        state = SearchState.initial;
+      } else {
+        state = SearchState.recent;
+      }
+    } else {
+      if (recentSearches.isEmpty) {
+        state = SearchState.noResult;
+      } else {
+        state = SearchState.success;
+      }
+    }
+
     return SliverToBoxAdapter(
-        child: switch (_state) {
-      SearchState.initial => _buildInitialSearch(context),
-      SearchState.recent => _buildRecentSearch(context),
-      SearchState.success => _buildSuccessSearch(context),
-      SearchState.noResult => _buildNoResultSearch(context),
-    });
+      child: switch (state) {
+        SearchState.initial => _buildInitialSearch(context),
+        SearchState.recent => _buildRecentSearch(context),
+        SearchState.success => _buildSuccessSearch(context),
+        SearchState.noResult => _buildNoResultSearch(context),
+      },
+    );
   }
 
   Widget _buildCategorySection(BuildContext context) {
@@ -156,7 +179,13 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   Widget _buildSuccessSearch(BuildContext context) {
-    return Center();
+    return Column(
+      children: [
+        _buildCategorySection(context),
+         GridProductsSection(),
+
+      ],
+    );
   }
 
   Widget _buildNoResultSearch(BuildContext context) {
@@ -189,3 +218,134 @@ class _SearchScreenState extends State<SearchScreen> {
 }
 
 enum SearchState { initial, recent, success, noResult }
+
+
+class GridProductsSection extends StatefulWidget {
+  const GridProductsSection({super.key});
+
+  @override
+  State<GridProductsSection> createState() => _GridProductsSectionState();
+}
+
+class _GridProductsSectionState extends State<GridProductsSection> {
+  final Map<int, int> _productCounts = {};
+  bool _isFavourite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> products = [
+      {
+        'id': 0,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Gold Stainless Steel Sun Charm Necklace',
+        'price': '\$5.00',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 1,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Birthday cake with bows',
+        'price': '\$12.99',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 3,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Product Name 3',
+        'price': '\$30.99',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 4,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Birthday cake with bows',
+        'price': '\$12.99',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 5,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Product Name 3',
+        'price': '\$30.99',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 6,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Product Name 3',
+        'price': '\$30.99',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 7,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Birthday cake with bows',
+        'price': '\$12.99',
+        'isFavourite': _isFavourite,
+      },
+      {
+        'id': 8,
+        'imageUrl': 'assets/images/product_3.webp',
+        'title': 'Product Name 3',
+        'price': '\$30.99',
+        'isFavourite': _isFavourite,
+      },
+    ];
+
+    void incrementProduct(int productId) {
+      setState(() {
+        _productCounts[productId] = (_productCounts[productId] ?? 0) + 1;
+      });
+    }
+
+    void decrementProduct(int productId) {
+      setState(() {
+        final count = _productCounts[productId] ?? 0;
+        if (count > 0) {
+          _productCounts[productId] = count - 1;
+        }
+      });
+    }
+
+    void toggleFavourite(int productId) {
+      setState(() {
+        _isFavourite = !_isFavourite;
+      });
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          itemCount: products.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 12,
+            childAspectRatio: 170 / 272,
+          ),
+          itemBuilder: (context, index) {
+            final product = products[index];
+            final productId = product['id'] as int;
+            final count = _productCounts[productId] ?? 0;
+
+            return ProductVerticalCard(
+              imageUrl: product['imageUrl'],
+              title: product['title'],
+              price: product['price'],
+              count: count,
+              isFavorite: product['isFavourite'],
+              onIncrement: () => incrementProduct(productId),
+              onDecrement: () => decrementProduct(productId),
+              onFavorite: () => toggleFavourite(productId),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
