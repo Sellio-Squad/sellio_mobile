89d73fddc1e137cada4e692061473bc4f29ddc90
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-11-10 02:05:40 +0300

    feat: Implement image picker for logo upload
    - Adds the `image_picker` dependency to `pubspec.yaml`.
    - Implements functionality to pick an image from the gallery in the `UploadLogoSection`.
    - Displays the selected image preview on the screen.
    - Adds necessary Android permissions for camera and storage access (`AndroidManifest.xml`).
    - Includes a `FileProvider` configuration for handling file URIs (`provider_paths.xml`).

--- lib/ui/screens/customize_your_product_screen/widgets/upload_logo_section.dart
+++ lib/ui/screens/customize_your_product_screen/widgets/upload_logo_section.dart
@@ -1,11 +1,29 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sellio_mobile/core/design_system/constants/assets.dart';
 import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
 
-import '../../../../core/design_system/constants/assets.dart';
-
-class UploadLogoSection extends StatelessWidget {
+class UploadLogoSection extends StatefulWidget {
   const UploadLogoSection({super.key});
+
+  @override
+  State<UploadLogoSection> createState() => _UploadLogoSectionState();
+}
+
+class _UploadLogoSectionState extends State<UploadLogoSection> {
+  File? _selectedImage;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,30 +38,47 @@ class UploadLogoSection extends StatelessWidget {
         ),
         const SizedBox(height: 12),
         GestureDetector(
-          onTap: () {
-            // TODO: handle image upload
-          },
+          onTap: _pickImage,
           child: Container(
             width: double.infinity,
-            height: 120,
+            height: 280,
             decoration: BoxDecoration(
               color: context.theme.colors.surface,
-              borderRadius: BorderRadius.circular(8),
+              borderRadius: BorderRadius.circular(12),
               border: Border.all(
                 color: context.theme.colors.disabled ?? Colors.grey[300]!,
                 width: 1,
               ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-                 SvgPicture.asset(
-                  Assets.upload,
-                   width: 48,
-                  height: 78,
-                  fit: BoxFit.scaleDown,
-                ),
-                ],
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                if (_selectedImage != null)
+                  Positioned(
+                    top: 80,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.file(
+                        _selectedImage!,
+                        width: 160,
+                        height: 160,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  )
+                else
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        Assets.upload,
+                        width: 48,
+                        height: 78,
+                        fit: BoxFit.scaleDown,
+                      ),
+                    ],
+                  ),
+              ],
             ),
           ),
         ),
