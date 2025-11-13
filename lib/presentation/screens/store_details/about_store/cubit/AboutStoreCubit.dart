45b8847db880dc0dd3453a72f10439941b1868be
Mohamed <m.3.abdelfadeel@gmail.com>
2025-11-13 20:07:21 +0200

    feat: add AboutStoreCubit

---
+++ lib/presentation/screens/store_details/about_store/cubit/AboutStoreCubit.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../domain/core/result.dart';
+import '../../../../../domain/repositories/store_repository.dart';
+import 'AboutStoreState.dart';
+
+class AboutStoreCubit extends Cubit<AboutStoreState> {
+  final StoreRepository _repository;
+
+  AboutStoreCubit(this._repository) : super(const AboutStoreInitial());
+
+  Future<void> loadStoreInfo(String storeId) async {
+    emit(const AboutStoreLoading());
+
+    final storeResult = await _repository.getStoreById(storeId);
+    final ratingResult = await _repository.getStoreRating(storeId);
+
+    if (storeResult is Success && ratingResult is Success) {
+      emit(AboutStoreLoaded(
+        store: storeResult.data,
+        rating: ratingResult.data,
+      ));
+    } else {
+      final errorMessage = _extractErrorMessage([storeResult, ratingResult]);
+      emit(AboutStoreError(message: errorMessage));
+    }
+  }
+
+  String _extractErrorMessage(List<Result> results) {
+    for (final r in results) {
+      if (r is ResultFailure) {
+        return r.failure.message;
+      }
+    }
+    return 'Something went wrong';
+  }
+}
