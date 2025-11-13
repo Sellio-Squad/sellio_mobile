489ae2afa2075e6414b94689cd6482d63f22bad6
MuhammedWael9991 <mw26200400@gmail.com>
2025-11-13 20:04:16 +0200

    Add localization support for Arabic and English

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,327 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get hello => 'مرحبًا';
+
+  @override
+  String get welcome => 'مرحبًا';
+
+  @override
+  String get top_stores => 'أعلى المتاجر';
+
+  @override
+  String get trending_products => 'Trending Products';
+
+  @override
+  String get search_results => 'Search Results';
+
+  @override
+  String get search_your_favorite_items => 'Search your favorite items...';
+
+  @override
+  String get special_offers => 'Special Offers';
+
+  @override
+  String get add_to_cart => 'Add to cart';
+
+  @override
+  String get reset => 'Reset';
+
+  @override
+  String get note_optional => 'Note  (Optional)';
+
+  @override
+  String get all => 'All';
+
+  @override
+  String get food => 'Food';
+
+  @override
+  String get drinks => 'Drinks';
+
+  @override
+  String get clothes => 'Clothes';
+
+  @override
+  String get thrift => 'Thrift';
+
+  @override
+  String get furniture => 'furniture';
+
+  @override
+  String get books => 'Books';
+
+  @override
+  String get electronics => 'Electronics';
+
+  @override
+  String get notifications => 'Notifications';
+
+  @override
+  String get account_screen => 'Account Screen';
+
+  @override
+  String get no_products_available => 'No products available';
+
+  @override
+  String get featured_items => 'Featured Items';
+
+  @override
+  String get has_been_placed_successfully => 'has been placed successfully';
+
+  @override
+  String get has_been_delivered_successfully =>
+      'has been delivered successfully';
+
+  @override
+  String get has_been_cancelled => 'has been cancelled';
+
+  @override
+  String get today => 'Today';
+
+  @override
+  String get yesterday => 'Yesterday';
+
+  @override
+  String get our_friendly_team_is_here_to_help =>
+      'Our friendly team is here to help';
+
+  @override
+  String get store_location => 'store_location';
+
+  @override
+  String get design_editor => 'Design editor';
+
+  @override
+  String get s => 'S';
+
+  @override
+  String get m => 'M';
+
+  @override
+  String get l => 'L';
+
+  @override
+  String get xl => 'XL';
+
+  @override
+  String get two_xl => '2XL';
+
+  @override
+  String get three_xl => '3XL';
+
+  @override
+  String get accessories_and_gifts => 'Accessories & Gifts';
+
+  @override
+  String get home_and_decore => 'Home & Decore';
+
+  @override
+  String get tech_accessories => 'Tech Accessories';
+
+  @override
+  String get business_and_branding => 'Business & Branding';
+
+  @override
+  String get customize_your_product => 'Customize your product';
+
+  @override
+  String get choose_a_product_to_customize_it =>
+      'Choose_a_product_to_customize_it';
+
+  @override
+  String get upload_logo_or_image => 'Upload logo or image';
+
+  @override
+  String get size => 'Size';
+
+  @override
+  String get color => 'Color';
+
+  @override
+  String get change_language => 'تغيير اللغة';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get arabic => 'Arabic';
+
+  @override
+  String get save => 'Save';
+
+  @override
+  String get delete_account => 'Delete account';
+
+  @override
+  String get account_options => 'account options';
+
+  @override
+  String get are_you_sure_to_continue_deleting_account =>
+      'Are you sure to continue deleting account?';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get are_you_sure_to_continue_logout =>
+      'Are you sure to continue logout?';
+
+  @override
+  String get start_exploring_and_saving_your_favorite_items_here =>
+      'Start exploring and saving your favorite items here.';
+
+  @override
+  String get no_favourite_items => 'No favourite items!';
+
+  @override
+  String get favorites => 'Favorites';
+
+  @override
+  String get products => 'Products';
+
+  @override
+  String get stores => 'Stores';
+
+  @override
+  String get recent_searches => 'Recent Searches';
+
+  @override
+  String get clear_all => 'Clear All';
+
+  @override
+  String get no_results_found => 'No results found!';
+
+  @override
+  String get please_check_your_spelling_or_try_a_different_search =>
+      'Please check your spelling or try a different search.';
+
+  @override
+  String get start_exploring_your_favorite_items =>
+      'Start exploring your favorite items!';
+
+  @override
+  String get iraq => 'Iraq';
+
+  @override
+  String get egypt => 'Egypt';
+
+  @override
+  String get confirm_your_account => 'Confirm your account';
+
+  @override
+  String get confirm => 'Confirm';
+
+  @override
+  String get dont_received_code => 'Don\\\'t received code?';
+
+  @override
+  String get re_send => 'Re-Send';
+
+  @override
+  String get confirm_password => 'Confirm Password';
+
+  @override
+  String get enter_code => 'Enter code';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forget_password => 'Forget Password?';
+
+  @override
+  String get phone_number_must_be_at_least_10_digits =>
+      'Phone number must be at least 10 digits';
+
+  @override
+  String get phone_number_digits_only =>
+      'Phone number must contain only digits';
+
+  @override
+  String get full_name_at_least_2_characters =>
+      'Full name must be at least 2 characters';
+
+  @override
+  String get full_name_letters_only =>
+      'Full name must contain only letters and spaces';
+
+  @override
+  String get country_at_least_2_characters =>
+      'Country must be at least 2 characters';
+
+  @override
+  String get country_letters_only =>
+      'Country must contain only letters and spaces';
+
+  @override
+  String get city_at_least_2_characters => 'City must be at least 2 characters';
+
+  @override
+  String get city_letters_only => 'City must contain only letters and spaces';
+
+  @override
+  String get password_min_6_characters =>
+      'Password must be at least 6 characters';
+
+  @override
+  String get password_max_20_characters =>
+      'Password must be less than 20 characters';
+
+  @override
+  String get passwords_do_not_match => 'Passwords do not match';
+
+  @override
+  String get failed_to_create_account =>
+      'Failed to create account. Please try again.';
+
+  @override
+  String get profile_photo_optional => 'Profile photo (optional)';
+
+  @override
+  String get enter_the_4digit =>
+      'Please enter the 4-digit code sent to your phone number.';
+
+  @override
+  String get no_order_history => 'No order history!';
+
+  @override
+  String get start_exploring => 'Start Exploring';
+
+  @override
+  String get start_exploring_favorite_items =>
+      'Start exploring and purchasing your favorite items';
+
+  @override
+  String total_reviews(Object totalReviews) {
+    return '(\$totalReviews reviews)';
+  }
+
+  @override
+  String no_products_found_for(Object search_query) {
+    return 'لا توجد منتجات لـ $search_query';
+  }
+
+  @override
+  String your_order_from(Object orderId) {
+    return 'Your order $orderId from ';
+  }
+
+  @override
+  String re_send_in_resend_countdown_Sec(Object resendCountdown) {
+    return 'Re-Send in \$resendCountdown Sec';
+  }
+
+  @override
+  String enter_the_4_digit_sent_to(Object phone_number) {
+    return 'Please enter the 4-digit code sent to \$$phone_number.';
+  }
+}
