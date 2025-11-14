3c2cde1c8f420ec4b5c0ce92982839a195169d65
abdo-essam <abdo.e.mosa@gmail.com>
2025-11-14 14:04:31 +0200

    refactor: Move CreateAccount screen to new directory
    Moves the `CreateAccount` screen and its related files to a new, more organized directory structure. The file `lib/presentation/screens/auth/createAccount.dart` has been deleted and its contents relocated.
    
    Additionally, an unused `.gitkeep` file has been removed from the `assets/images` directory.

--- lib/core/app_management/route/route_manager.dart
+++ lib/core/app_management/route/route_manager.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sellio_mobile/core/app_management/route/route_args.dart';
 import '../../../presentation/screens/auth/create_account/create_account_screen.dart';
-import '../../../presentation/screens/auth/createAccount.dart';
 import '../../../presentation/screens/auth/forgetPassword/confirm_password_screen.dart';
 import '../../../presentation/screens/auth/forgetPassword/forget_password_otp_screen.dart';
 import '../../../presentation/screens/auth/forgetPassword/forget_password_screen.dart';
