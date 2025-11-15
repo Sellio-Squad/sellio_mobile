b1eb6db272d02d1d30fa0ac54c68317935861a7d
Arzo-zi <zinahashraf88@gmail.com>
2025-11-15 04:28:56 +0300

    refactor: product details screen state management

--- lib/presentation/screens/product_details/ProductDetailsScreen.dart
+++ lib/presentation/screens/product_details/ProductDetailsScreen.dart
@@ -1,295 +1,142 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sellio_mobile/core/design_system/constants/app_strings.dart';
 import 'package:sellio_mobile/core/design_system/constants/assets.dart';
 import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
 import 'package:sellio_mobile/core/design_system/widgets/buttons/button.dart';
 import 'package:sellio_mobile/core/design_system/widgets/textField.dart';
+import 'package:sellio_mobile/core/design_system/widgets/sellio_app_bar.dart';
+import 'package:sellio_mobile/domain/entities/product.dart';
+import 'package:sellio_mobile/domain/repositories/product_repository.dart';
+import 'package:sellio_mobile/presentation/cubits/cart/cubit/cart_cubit.dart';
+import 'package:sellio_mobile/presentation/cubits/cart/cubit/cart_state.dart';
+import 'package:sellio_mobile/presentation/screens/product_details/cubit/product_details_cubit.dart';
+import 'package:sellio_mobile/presentation/screens/product_details/cubit/product_details_state.dart';
+import 'package:sellio_mobile/presentation/screens/product_details/widgets/product_counter_section.dart';
+import 'package:sellio_mobile/presentation/screens/product_details/widgets/product_favorite.dart';
+import 'package:sellio_mobile/presentation/screens/product_details/widgets/product_image_section.dart';
+import 'package:sellio_mobile/presentation/screens/product_details/widgets/product_price_section.dart';
 
-import '../../../core/design_system/widgets/sellio_app_bar.dart';
-
-class ProductDetailsScreen extends StatefulWidget {
-  final int productCount;
-  final String productDescription;
-  final double productPrice;
-
-  final double? productPriceBeforeDiscount;
+class ProductDetailsScreen extends StatelessWidget {
+  final String productId;
 
   const ProductDetailsScreen({
     super.key,
-    required this.productCount,
-    required this.productDescription,
-    required this.productPrice,
-    this.productPriceBeforeDiscount,
+    required this.productId,
   });
 
   @override
-  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
-}
-
-class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
-  bool _isFavorite = false;
-  late int productCount = widget.productCount;
-  String noteText = '';
-
-  void _toggleFavorite() {
-    setState(() {
-
-      _isFavorite = !_isFavorite;
-    });
-  }
-
-  void incrementProduct() {
-    setState(() {
-
-      productCount++;
-    });
-  }
-
-  void decrementProduct() {
-    setState(() {
-
-      if (productCount > 0) {
-        productCount--;
-      }
-    });
-  }
-
-  void productNote() {
-    setState(() {
-      noteText = '';
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.theme.colors.surfaceLow,
-      appBar: SellioAppBar(
-        showBackButton: true,
-        title: 'Fresh Lemon Cake',
-        actions: [
-          IconButton(
-            icon: SvgPicture.asset(
-              _isFavorite ? Assets.favorite : Assets.unselectedFavorite,
-              width: 28,
-              height: 28,
-            ),
-            onPressed: _toggleFavorite,
-            padding: EdgeInsets.zero,
+    final product = Product.dummy();
+
+    return BlocProvider(
+      create: (context) => ProductDetailsCubit(
+        context.read<ProductRepository>(),
+      )..loadProductDetails(productId),
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          backgroundColor: context.theme.colors.surfaceLow,
+          appBar: SellioAppBar(
+            showBackButton: true,
+            title: product.name,
+            actions: [
+              productFavorite(context, productId),
+            ],
           ),
-        ],
-      ),
-
-      body: Column(
-        children: [
-          _productImage(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
+          body: Column(
+            children: [
+              productImagesSection(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(
-                      widget.productPriceBeforeDiscount == null
-                          ? ''
-                          : '\$${widget.productPriceBeforeDiscount}',
-                      style: context.theme.typography.textTheme.titleSmall
-                          .copyWith(
-                            color: context.theme.colors.hint,
-                            decoration: TextDecoration.lineThrough,
-                            decorationColor: context.theme.colors.hint,
-                          ),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(left: 3),
-                      child: Text(
-                        '\$${widget.productPrice}',
-                        style: context.theme.typography.textTheme.titleSmall
-                            .copyWith(color: context.theme.colors.primary),
-                      ),
-                    ),
-                    const Expanded(child: Spacer()),
-                    GestureDetector(
-                      onTap: decrementProduct,
-                      child: Container(
-                        width: 28,
-                        height: 28,
-                        decoration: BoxDecoration(
-                          color: context.theme.colors.surface,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset(
-                              Assets.remove,
-                              width: 16,
-                              height: 16,
-                              colorFilter: ColorFilter.mode(
-                                context.theme.colors.body,
-                                BlendMode.srcIn,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 3,
-                        horizontal: 8,
-                      ),
-                      child: Text(
-                        productCount < 10 ? '0$productCount' : '$productCount',
-                        style: context.theme.typography.textTheme.labelMedium
-                            .copyWith(color: context.theme.colors.title),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: incrementProduct,
-                      child: Container(
-                        width: 28,
-                        height: 28,
-                        decoration: BoxDecoration(
-                          color: context.theme.colors.primaryVariant,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset(
-                              Assets.add,
-                              width: 16,
-                              height: 16,
-                              colorFilter: ColorFilter.mode(
-                                context.theme.colors.primary,
-                                BlendMode.srcIn,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    _buildPriceAndCounterRow(context),
+                    _buildDescription(context),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Text(
-                    widget.productDescription,
-                    style: context.theme.typography.textTheme.bodyMedium
-                        .copyWith(color: context.theme.colors.body),
-                  ),
-                ),
-              ],
-            ),
+              ),
+              _buildNoteTextField(context),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: SellioTextField(
-              isParagraph: true,
-              hintText: AppStrings.noteOptional,
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 32, left: 16, right: 16),
-        child: SellioButton(
-          text: AppStrings.addToCart,
-          onTap: () {},
-          suffixSvgPath: Assets.cart,
+          bottomNavigationBar: _buildAddToCartButton(context),
         ),
       ),
     );
   }
 }
 
-Widget _productImage(){
-  return  Padding(
-    padding: const EdgeInsets.all(16),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                width: 110,
-                height: 110,
-                color: Colors.grey[200],
-                child: Image.asset(
-                  'assets/images/lemon_popsicle.jpg',
-                  width: 110,
-                  height: 110,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Center(
-                      child: Icon(
-                        Icons.image_outlined,
-                        size: 40,
-                        color: Colors.grey[400],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 4),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                width: 110,
-                height: 110,
-                color: Colors.grey[200],
-                child: Image.asset(
-                  'assets/images/lemon_cheesecake.jpg',
-                  width: 110,
-                  height: 110,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Center(
-                      child: Icon(
-                        Icons.image_outlined,
-                        size: 40,
-                        color: Colors.grey[400],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(width: 4),
-        Expanded(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Container(
-              height: 224,
-              color: Colors.grey[200],
-              child: Image.asset(
-                'assets/images/lemon_cake_main.jpg',
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Center(
-                    child: Icon(
-                      Icons.image_outlined,
-                      size: 60,
-                      color: Colors.grey[400],
-                    ),
-                  );
-                },
-              ),
-            ),
+  Widget _buildPriceAndCounterRow(BuildContext context) {
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, cartState) {
+        return BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+          builder: (context, productState) {
+            if (productState is! ProductDetailsLoading) return const SizedBox();
+
+            final productId = productState.product.id;
+            final count = cartState.productCounts[productId] ?? 0;
+
+            return Row(
+              children: [
+                productPriceSection(context, productState),
+                const Expanded(child: Spacer()),
+                productCounterSection(context, productId, count),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildDescription(BuildContext context) {
+    return BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+      builder: (context, state) {
+        if (state is! ProductDetailsLoading) return const SizedBox();
+
+        return Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: Text(
+            state.product.description,
+            style: context.theme.typography.textTheme.bodyMedium
+                .copyWith(color: context.theme.colors.body),
           ),
-        ),
-      ],
-    ),
-  );
-}
+        );
+      },
+    );
+  }
+
+  Widget _buildNoteTextField(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+        builder: (context, state) {
+          if (state is! ProductDetailsLoading) return const SizedBox();
+
+          return SellioTextField(
+            isParagraph: true,
+            hintText: AppStrings.noteOptional,
+            controller: context.read<ProductDetailsCubit>().noteController,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildAddToCartButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 32, left: 16, right: 16),
+      child: BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+        builder: (context, state) {
+          if (state is! ProductDetailsLoading) return const SizedBox();
+
+          return SellioButton(
+            text: AppStrings.addToCart,
+            onTap: () => context.read<ProductDetailsCubit>().addToCart(),
+            suffixSvgPath: Assets.cart,
+          );
+        },
+      ),
+    );
+  }
