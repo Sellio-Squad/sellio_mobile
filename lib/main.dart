6db1ba9fe5bc05a7f72304ac643b81882c24d7ec
Mohamed <m.3.abdelfadeel@gmail.com>
2025-11-18 19:47:28 +0200

    feat: add store dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sellio_mobile/core/design_system/themes/sellio_theme_provider.dart';
 import 'package:sellio_mobile/presentation/screens/order_history/cubit/order_history_cubit.dart';
+import 'package:sellio_mobile/presentation/screens/store_details/cubit/store_details_cubit.dart';
 
 import 'core/di/injection_container.dart' as di;
 import 'core/localization/l10n/app_localizations.dart';
 import 'core/navigate/route_manager.dart';
 import 'domain/repositories/category_repository.dart';
 import 'domain/repositories/product_repository.dart';
+import 'domain/repositories/store_repository.dart';
 import 'presentation/cubits/cart/cubit/cart_cubit.dart';
 import 'presentation/cubits/favorites/cubit/favorites_cubit.dart';
 import 'presentation/cubits/user/cubit/user_cubit.dart';
@@ -34,6 +36,7 @@ class MyApp extends StatelessWidget {
       providers: [
         RepositoryProvider(create: (_) => di.sl<ProductRepository>()),
         RepositoryProvider(create: (_) => di.sl<CategoryRepository>()),
+        RepositoryProvider(create: (_) => di.sl<StoreRepository>()),
       ],
       child: MultiBlocProvider(
         providers: [
@@ -41,6 +44,7 @@ class MyApp extends StatelessWidget {
           BlocProvider(create: (_) => di.sl<FavoritesCubit>()),
           BlocProvider(create: (_) => di.sl<OrderHistoryCubit>()),
           BlocProvider(create: (_) => di.sl<UserCubit>()),
+          BlocProvider(create: (_) => di.sl<StoreDetailsCubit>()),
         ],
         child: MaterialApp.router(
           debugShowCheckedModeBanner: false,
