9ccb8ebe952c3c27273fb3fc92e64fe92129c2d0
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-11-22 16:09:59 +0200

    refactor(di): Comment out unused repository registrations

--- lib/di/modules/repository_module.dart
+++ lib/di/modules/repository_module.dart
@@ -1,6 +1,5 @@
 import 'package:get_it/get_it.dart';
 import 'package:sellio_mobile/domain/repositories/notification_repository.dart';
-
 import '../../../data/repositories/auth_repository_impl.dart';
 import '../../../data/repositories/cart_repository_impl.dart';
 import '../../../data/repositories/category_repository_impl.dart';
@@ -23,70 +22,69 @@ import '../../../domain/repositories/user_repository.dart';
 
 class RepositoryModule {
   static void register(GetIt sl) {
-    sl.registerLazySingleton<AuthRepository>(
-          () => AuthRepositoryImpl(
-        remoteDataSource: sl(),
-        storageService: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<ProductRepository>(
-          () => ProductRepositoryImpl(
-        remoteDataSource: sl(),
-        favoritesRemoteDataSource: sl(),
-        storageService: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<StoreRepository>(
-          () => StoreRepositoryImpl(
-        remoteDataSource: sl(),
-        favoritesRemoteDataSource: sl(),
-        storageService: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<CategoryRepository>(
-          () => CategoryRepositoryImpl(
-        remoteDataSource: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<CartRepository>(
-          () => CartRepositoryImpl(
-        remoteDataSource: sl(),
-        storageService: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<OrderRepository>(
-          () => OrderRepositoryImpl(
-        remoteDataSource: sl(),
-      ),
-    );
+    // sl.registerLazySingleton<AuthRepository>(
+    //       () => AuthRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //     storageService: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<ProductRepository>(
+    //       () => ProductRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //     favoritesRemoteDataSource: sl(),
+    //     storageService: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<StoreRepository>(
+    //       () => StoreRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //     favoritesRemoteDataSource: sl(),
+    //     storageService: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<CategoryRepository>(
+    //       () => CategoryRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<CartRepository>(
+    //       () => CartRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //     storageService: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<OrderRepository>(
+    //       () => OrderRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //   ),
+    // );
 
     sl.registerLazySingleton<UserRepository>(
           () => UserRepositoryImpl(
-        remoteDataSource: sl(),
-        storageService: sl(),
+        remoteDataSource: sl()
       ),
     );
-
-    sl.registerLazySingleton<FavoritesRepository>(
-          () => FavoritesRepositoryImpl(
-        remoteDataSource: sl(),
-        storageService: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<OffersRepository>(
-          () => OffersRepositoryImpl(
-        remoteDataSource: sl(),
-      ),
-    );
-
-    sl.registerLazySingleton<NotificationRepository>(
-          () => NotificationRepositoryImpl(),
-    );
+    //
+    // sl.registerLazySingleton<FavoritesRepository>(
+    //       () => FavoritesRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //     storageService: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<OffersRepository>(
+    //       () => OffersRepositoryImpl(
+    //     remoteDataSource: sl(),
+    //   ),
+    // );
+    //
+    // sl.registerLazySingleton<NotificationRepository>(
+    //       () => NotificationRepositoryImpl(),
+    // );
   }
 }
