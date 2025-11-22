7c9a6ee14731b9f8a4eac805255f6bc28672123a
MuhammedWael9991 <mw26200400@gmail.com>
2025-11-22 17:22:42 +0200

    add navigation functions for orders, favourites, and app version

---
+++ lib/presentation/screens/account/navigation/account_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../../../../core/navigate/navigation_extensions.dart';
+
+
+void navigateToMyOrders(BuildContext context) {
+  context.navigator.pushMyOrders();
+}
+
+void navigateToMyFavourites(BuildContext context) {
+  // TODO: Implement app version navigation
+}
+
+void navigateToAppVersion(BuildContext context) {
+  // TODO: Implement app version navigation
+}
