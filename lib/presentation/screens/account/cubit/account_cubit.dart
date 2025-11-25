40d1a7f1ab5d27494b87a6697047431004309494
MuhammedWael9991 <mw26200400@gmail.com>
2025-11-25 03:37:42 +0200

    add profile picture update functionality in AccountCubit

--- lib/presentation/screens/account/cubit/account_cubit.dart
+++ lib/presentation/screens/account/cubit/account_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sellio_mobile/domain/repositories/user_repository.dart';
 import 'package:sellio_mobile/presentation/screens/account/cubit/account_state.dart';
 
@@ -24,6 +25,34 @@ class AccountCubit extends Cubit<AccountState> {
       final errorMessage = _extractErrorMessage([result]);
       emit(AccountError(message: errorMessage));
     }
+  }
+
+  Future<void> updateProfilePicture() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    // if (pickedFile != null) {
+    //   emit(const AccountLoading());
+    //   final uploadResult = await _repository.uploadProfilePhoto(pickedFile.path);
+    //   if (uploadResult is Success) {
+    //     final updateResult = await _repository.updateUserProfile(avatarUrl: uploadResult.data);
+    //     if (updateResult is Success) {
+    //       emit(AccountLoaded(
+    //         firstName: updateResult.data.firstName,
+    //         lastName: updateResult.data.lastName,
+    //         email: updateResult.data.email,
+    //         imagePath: updateResult.data.avatarUrl,
+    //       ));
+    //     } else {
+    //       final errorMessage = _extractErrorMessage([updateResult]);
+    //       emit(AccountError(message: errorMessage));
+    //     }
+    //   } else {
+    //     final errorMessage = _extractErrorMessage([uploadResult]);
+    //     emit(AccountError(message: errorMessage));
+    //   }
+    // }
+
   }
 
   String _extractErrorMessage(List<Result> results) {
