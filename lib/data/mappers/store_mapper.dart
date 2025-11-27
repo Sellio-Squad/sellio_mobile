9b8724424ea1a1893c08435883684aa80c8f3e3e
Karrar Abbas <54599574+karrar-abbas@users.noreply.github.com>
2025-11-27 22:53:34 +0300

    refactor: get store top rating  from api

--- lib/data/mappers/store_mapper.dart
+++ lib/data/mappers/store_mapper.dart
@@ -1,10 +1,12 @@
 import 'package:sellio_mobile/data/mappers/review_mapper.dart';
+import 'package:sellio_mobile/data/models/store_top_rating_model.dart';
+import 'package:sellio_mobile/domain/entities/address.dart';
 
+import '../../domain/entities/store.dart';
+import '../models/store_model.dart';
 import 'address_mapper.dart';
 import 'category_mapper.dart';
 import 'contact_info_mapper.dart';
-import '../models/store_model.dart';
-import '../../domain/entities/store.dart';
 
 extension StoreModelMapper on StoreModel {
   Store toEntity() => Store(
@@ -21,4 +23,21 @@ extension StoreModelMapper on StoreModel {
     reviews: reviews.map((e) => e.toEntity()).toList(),
     isActive: isActive,
   );
+}
+
+extension StoreItemMapper on StoreTopRatingModel {
+  Store toEntity() => Store(
+        id: id,
+        name: title,
+        description: '',
+        coverImage: coverImageURL ?? "",
+        profileImage: "",
+        sale: maxDiscount?.toString(),
+        rating: 0.0,
+        address: Address(country: "", city: ""),
+        contactInfoList: [],
+        categories: [],
+        reviews: [],
+        isActive: true,
+      );
 }
