e01ac5a8df7b69e83b28ade34e1715709b84c532
Karrar Abbas <54599574+karrar-abbas@users.noreply.github.com>
2025-11-27 23:24:20 +0300

    Feat/239 replace store mock data with api (#307)
    * refactor: get store top rating  from api
    
    * fix: merging issues

--- lib/di/mock/MockRepositoryModule.dart
+++ lib/di/mock/MockRepositoryModule.dart
@@ -1,25 +1,22 @@
 import 'package:get_it/get_it.dart';
-import '../../data/mock/repositories_impl/mock_auth_repository_impl.dart';
+
 import '../../data/mock/repositories_impl/mock_cart_repository_impl.dart';
 import '../../data/mock/repositories_impl/mock_category_repository_impl.dart';
 import '../../data/mock/repositories_impl/mock_favorites_repository_impl.dart';
 import '../../data/mock/repositories_impl/mock_notification_repository_impl.dart';
 import '../../data/mock/repositories_impl/mock_offers_repository_impl.dart';
 import '../../data/mock/repositories_impl/mock_product_repository_impl.dart';
-import '../../data/mock/repositories_impl/mock_store_repository_impl.dart';
-import '../../domain/repositories/auth_repository.dart';
 import '../../domain/repositories/cart_repository.dart';
 import '../../domain/repositories/favorites_repository.dart';
 import '../../domain/repositories/notification_repository.dart';
 import '../../domain/repositories/offers_repository.dart';
 import '../../domain/repositories/product_repository.dart';
-import '../../domain/repositories/store_repository.dart';
 
 class MockRepositoryModule {
   static void register(GetIt sl) {
-    sl.registerLazySingleton<AuthRepository>(() => MockAuthRepositoryImpl());
+    // sl.registerLazySingleton<AuthRepository>(() => MockAuthRepositoryImpl());
     sl.registerLazySingleton<ProductRepository>(() => MockProductRepositoryImpl());
-    sl.registerLazySingleton<StoreRepository>(() => MockStoreRepositoryImpl());
+    // sl.registerLazySingleton<StoreRepository>(() => MockStoreRepositoryImpl());
     sl.registerLazySingleton<CartRepository>(() => MockCartRepositoryImpl());
     // sl.registerLazySingleton<CategoryRepository>(() => MockCategoryRepositoryImpl());
     sl.registerLazySingleton<FavoritesRepository>(() => MockFavoritesRepositoryImpl());
