01bfd0e0a166030c6f900b49fc0f29c57ed75477
abdo-essam <abdo.e.mosa@gmail.com>
2025-12-06 22:54:41 +0200

    refactor: Remove related feature files

--- lib/domain/repositories/category_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../../core/error/result.dart';
-import '../entities/category.dart';
-
-abstract class CategoryRepository {
-  Future<Result<List<Category>>> getCategories();
-
-  Future<Result<Category>> getCategoryById(String categoryId);
-}
--- lib/presentation/utils/product_price_extensions.dart
+++
@@ -1,9 +0,0 @@
-extension ProductPriceExtensions on double {
-  String oldPrice(int? discountPercentage) {
-    if (discountPercentage == null || discountPercentage <= 0) {
-      return toStringAsFixed(2);
-    }
-    final double originalPrice = this / (1 - discountPercentage / 100);
-    return originalPrice.toStringAsFixed(2);
-  }
-}
