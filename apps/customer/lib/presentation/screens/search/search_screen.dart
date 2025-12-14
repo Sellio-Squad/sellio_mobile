55e86733dd458d4b15f6d363c3633bdb5cf27984
yaseenOmar <yz456359@gmail.com>
2025-12-14 19:42:39 +0200

    Refactor search screen with Cubit

--- apps/customer/lib/presentation/screens/search/search_screen.dart
+++ apps/customer/lib/presentation/screens/search/search_screen.dart
@@ -1,45 +1,33 @@
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gap/flutter_gap.dart';
-import 'package:design_system/design_system.dart';
-import 'package:design_system/design_system.dart';
-import 'package:design_system/design_system.dart';
 import 'package:sellio_mobile/core/localization/l10n/localization_service.dart';
-import 'package:design_system/design_system.dart';
-import 'package:design_system/design_system.dart';
 import '../../../presentation/screens/home/sections/search/widgets/search_bar_widget.dart';
+import '../../cubits/search/cubit/search_cubit.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => SearchCubit()..init(),
+      child: const _SearchView(),
+    );
+  }
 }
 
-class _SearchScreenState extends State<SearchScreen> {
-  String _searchText = '';
-  String _selectedCategory = 'Products';
-  final TextEditingController _searchController = TextEditingController();
-
-  final List<String> recentSearches =
-  [
-    'Cake',
-    'Donut',
-    'Cola Diet',
-    'Lemonade',
-    'مرطب سميل',
-    'Cat accessories',
-    'عباية خليجي',
-  ];
+class _SearchView extends StatefulWidget {
+  const _SearchView();
 
   @override
-  void initState() {
-    super.initState();
-    _searchController.addListener(() {
-      setState(() {
-        _searchText = _searchController.text;
-      });
-    });
-  }
+  State<_SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<_SearchView> {
+  final TextEditingController _searchController = TextEditingController();
+  String _selectedCategory = 'Products';
 
   @override
   void dispose() {
@@ -49,117 +37,70 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<SearchCubit>();
+
     return Scaffold(
-        backgroundColor: context.theme.colors.surfaceLow,
-        appBar: SellioAppBar(
-          title: context.local.search,
-          showBackButton: true,
-        ),
-        body: CustomScrollView(slivers: [
-          _buildSearchBarSection(context),
-          _buildSearchBodySection(context),
-        ]));
-  }
-
-  SliverToBoxAdapter _buildSearchBarSection(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-        ),
-        child: SearchBarWithFilter(
-            onFilterIconClicked: () {},
-            onTextSubmitted: (String text) {
-            },
-            controller: _searchController),
+      backgroundColor: context.theme.colors.surfaceLow,
+      appBar: SellioAppBar(
+        title: context.local.search,
+        showBackButton: true,
       ),
-    );
-  }
-
-  SliverToBoxAdapter _buildSearchBodySection(BuildContext context) {
-    late final SearchState state;
-
-    if (_searchText.isEmpty) {
-      if (recentSearches.isEmpty) {
-        state = SearchState.initial;
-      } else {
-        state = SearchState.recent;
-      }
-    } else {
-      if (recentSearches.isEmpty) {
-        state = SearchState.noResult;
-      } else {
-        state = SearchState.success;
-      }
-    }
-
-    return SliverToBoxAdapter(
-      child: switch (state) {
-        SearchState.initial => _buildInitialSearch(context),
-        SearchState.recent => _buildRecentSearch(context),
-        SearchState.success => _buildSuccessSearch(context),
-        SearchState.noResult => _buildNoResultSearch(context),
-      },
-    );
-  }
-
-  Widget _buildCategorySection(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
-      child: Row(
-        children: [
-          SellioChip(
-            label: context.local.products,
-            selected: _selectedCategory == 'Products',
-            onTap: () {
-              setState(() {
-                _selectedCategory = 'Products';
-              });
+      body: CustomScrollView(
+        slivers: [
+          _buildSearchBarSection(context, cubit),
+          BlocBuilder<SearchCubit, SearchState>(
+            builder: (context, state) {
+              return SliverToBoxAdapter(
+                child: switch (state) {
+                  SearchInitial() => _buildInitialSearch(context),
+                  SearchRecent(:final recentSearches) =>
+                    _buildRecentSearch(context, recentSearches, cubit),
+                  SearchSuccess() => _buildSuccessSearch(context),
+                  SearchEmpty() => _buildNoResultSearch(context),
+                  // TODO: Handle this case.
+                  SearchState() => throw UnimplementedError(),
+                },
+              );
             },
-            assetIcon: AppImages.orderIcon,
-          ),
-          const Gap(8),
-          SellioChip(
-            label: context.local.stores,
-            selected: _selectedCategory == 'Stores',
-            onTap: () {
-              setState(() {
-                _selectedCategory = 'Stores';
-              });
-            },
-            assetIcon: AppImages.storeIcon,
           ),
         ],
       ),
     );
   }
 
-  Widget _buildRecentSearch(BuildContext context) {
+  SliverToBoxAdapter _buildSearchBarSection(
+      BuildContext context, SearchCubit cubit) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SearchBarWithFilter(
+          controller: _searchController,
+          onFilterIconClicked: () {},
+          onTextSubmitted: cubit.search,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRecentSearch(
+    BuildContext context,
+    List<String> recentSearches,
+    SearchCubit cubit,
+  ) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                context.local.recent_searches,
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: context.theme.colors.title,
-                    ),
-              ),
+              Text(context.local.recent_searches),
               GestureDetector(
-                onTap: () {
-                  setState(() {
-                    recentSearches.clear();
-                  });
-                },
+                onTap: cubit.clearRecent,
                 child: Text(
                   context.local.clear_all,
-                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                        color: context.theme.colors.primary,
-                      ),
+                  style: TextStyle(color: context.theme.colors.primary),
                 ),
               ),
             ],
@@ -170,14 +111,13 @@ class _SearchScreenState extends State<SearchScreen> {
             runSpacing: 4,
             children: recentSearches.map((text) {
               return SellioChip(
-                  label: text,
-                  selected: false,
-                  onTap: () {
-                    setState(() {
-                      _searchText = text;
-                      _searchController.text = text;
-                    });
-                  });
+                label: text,
+                selected: false,
+                onTap: () {
+                  _searchController.text = text;
+                  cubit.selectRecent(text);
+                },
+              );
             }).toList(),
           ),
         ],
@@ -189,16 +129,12 @@ class _SearchScreenState extends State<SearchScreen> {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.5,
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(AppImages.searchIcon),
-            Text(
-            context.local.start_exploring_your_favorite_items,
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: context.theme.colors.title,
-                    )),
-          ]),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(AppImages.searchIcon),
+          Text(context.local.start_exploring_your_favorite_items),
+        ],
+      ),
     );
   }
 
@@ -206,172 +142,43 @@ class _SearchScreenState extends State<SearchScreen> {
     return Column(
       children: [
         _buildCategorySection(context),
-         GridProductsSection(),
-
+        // TODO implement search result here
       ],
     );
   }
 
   Widget _buildNoResultSearch(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
+    return Column(
+      children: [
+        _buildCategorySection(context),
+        const Gap(24),
+        Image.asset(AppImages.noResultSearchIcon),
+        Text(context.local.no_results_found),
+        Text(
+          context.local.please_check_your_spelling_or_try_a_different_search,
+        ),
+      ],
+    );
+  }
 
-    return SizedBox(
-      height: height * 0.75,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+  Widget _buildCategorySection(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
         children: [
-          _buildCategorySection(context),
-          const Spacer(),
-          Image.asset(
-            AppImages.noResultSearchIcon,
+          SellioChip(
+            label: context.local.products,
+            selected: _selectedCategory == 'Products',
+            onTap: () => setState(() => _selectedCategory = 'Products'),
           ),
-          Text(
-              context.local.no_results_found,
-              style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                    color: context.theme.colors.title,
-                  )),
-
-          Text(
-              context.local.please_check_your_spelling_or_try_a_different_search,
-              style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                    color: context.theme.colors.body,
-                  )),
-          Spacer()
+          const Gap(8),
+          SellioChip(
+            label: context.local.stores,
+            selected: _selectedCategory == 'Stores',
+            onTap: () => setState(() => _selectedCategory = 'Stores'),
+          ),
         ],
       ),
     );
   }
 }
-
-enum SearchState { initial, recent, success, noResult }
-
-
-class GridProductsSection extends StatefulWidget {
-  const GridProductsSection({super.key});
-
-  @override
-  State<GridProductsSection> createState() => _GridProductsSectionState();
-}
-
-class _GridProductsSectionState extends State<GridProductsSection> {
-  final Map<int, int> _productCounts = {};
-  bool _isFavourite = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> products = [
-      {
-        'id': 0,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Gold Stainless Steel Sun Charm Necklace',
-        'price': '\$5.00',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 1,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Birthday cake with bows',
-        'price': '\$12.99',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 3,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Product Name 3',
-        'price': '\$30.99',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 4,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Birthday cake with bows',
-        'price': '\$12.99',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 5,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Product Name 3',
-        'price': '\$30.99',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 6,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Product Name 3',
-        'price': '\$30.99',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 7,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Birthday cake with bows',
-        'price': '\$12.99',
-        'isFavourite': _isFavourite,
-      },
-      {
-        'id': 8,
-        'imageUrl': 'assets/images/product_3.webp',
-        'title': 'Product Name 3',
-        'price': '\$30.99',
-        'isFavourite': _isFavourite,
-      },
-    ];
-
-    void incrementProduct(int productId) {
-      setState(() {
-        _productCounts[productId] = (_productCounts[productId] ?? 0) + 1;
-      });
-    }
-
-    void decrementProduct(int productId) {
-      setState(() {
-        final count = _productCounts[productId] ?? 0;
-        if (count > 0) {
-          _productCounts[productId] = count - 1;
-        }
-      });
-    }
-
-    void toggleFavourite(int productId) {
-      setState(() {
-        _isFavourite = !_isFavourite;
-      });
-    }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        GridView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          itemCount: products.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 12,
-            childAspectRatio: 170 / 272,
-          ),
-          itemBuilder: (context, index) {
-            final product = products[index];
-            final productId = product['id'] as int;
-            final count = _productCounts[productId] ?? 0;
-
-            return SellioProductVerticalCard(
-              imageUrl: product['imageUrl'],
-              title: product['title'],
-              price: product['price'],
-              count: count,
-              isFavorite: product['isFavourite'],
-              onIncrement: () => incrementProduct(productId),
-              onDecrement: () => decrementProduct(productId),
-              onFavorite: () => toggleFavourite(productId),
-            );
-          },
-        ),
-      ],
-    );
-  }
-}
-
