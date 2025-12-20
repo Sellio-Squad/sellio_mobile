bf15c7948f9d67bad0263a1ea97cb5153d0f9ff4
yaseenOmar <yz456359@gmail.com>
2025-12-20 07:09:33 +0200

    Implement recent search history using SharedPreferences

--- apps/customer/lib/presentation/screens/search/cubit/search_cubit.dart
+++ apps/customer/lib/presentation/screens/search/cubit/search_cubit.dart
@@ -2,20 +2,20 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
 import 'package:sellio_mobile/domain/entities/product.dart';
 import 'package:sellio_mobile/domain/repositories/product_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../core/error/result.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-   final ProductRepository _productRepository;
+  final ProductRepository _productRepository;
+
   SearchCubit(this._productRepository) : super(const SearchInitial());
 
-  final List<String> _recentSearches = [
-    "Cake",
-    "shoes",
-  ];
+  late List<String> _recentSearches;
 
-  void init() {
+  Future<void> init() async {
+    _recentSearches = await getRecentSearch();
     if (_recentSearches.isEmpty) {
       emit(const SearchInitial());
     } else {
@@ -23,32 +23,49 @@ class SearchCubit extends Cubit<SearchState> {
     }
   }
 
-  void search(String text) async {
+  Future<void> search(String text) async {
     if (text.isEmpty) {
       init();
       return;
     }
     final result = await _productRepository.searchProducts(query: text);
 
-
     if (result.isSuccess) {
       final products = (result as Success<List<Product>>).data;
       if (products.isNotEmpty) {
+        await saveRecentSearch(text);
         emit(SearchSuccess(products));
-      }else {
+      } else {
         emit(SearchEmpty());
       }
-
-
     }
   }
 
   void clearRecent() {
-    _recentSearches.clear();
+    clearRecentSearch();
     emit(const SearchInitial());
   }
 
   void selectRecent(String text) {
     search(text);
   }
-}
+
+  Future<void> saveRecentSearch(String query) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final List<String> recent = prefs.getStringList('recent_search') ?? [];
+
+    recent.insert(0, query);
+
+    await prefs.setStringList('recent_search', recent);
+  }
+
+  Future<List<String>> getRecentSearch() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('recent_search') ?? [];
+  }
+  Future<void> clearRecentSearch() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('recent_search');
+  }
+  }
