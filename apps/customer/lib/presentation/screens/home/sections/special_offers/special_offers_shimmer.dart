7a7f2abbf3378bc32a9afe0a3798367c0427f66e
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-12-21 23:58:41 +0200

    feat: Implement responsive offers and Implement carousel

--- apps/customer/lib/presentation/screens/home/sections/special_offers/special_offers_shimmer.dart
+++ apps/customer/lib/presentation/screens/home/sections/special_offers/special_offers_shimmer.dart
@@ -1,35 +1,36 @@
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
-import 'package:design_system/design_system.dart';
 import '../../widgets/section_header_shimmer.dart';
 
 class SpecialOffersShimmer extends StatelessWidget {
-  const SpecialOffersShimmer({super.key});
+  final double height;
+
+  const SpecialOffersShimmer({super.key, this.height = 200});
 
   @override
   Widget build(BuildContext context) {
     final theme = SellioTheme.of(context);
     final colors = theme.colors;
-    return Column(
-        children: [
-          // Shimmer for Section Header
-          const SectionHeaderShimmer(),
-          const SizedBox(height: 8),
-          SizedBox(
-            height: 200,
-            child: Shimmer.fromColors(
-              baseColor: colors.surface,
-              highlightColor: colors.surfaceLow,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
+    return Column(children: [
+      // Shimmer for Section Header
+      const SectionHeaderShimmer(),
+      const SizedBox(height: 8),
+      SizedBox(
+        height: height,
+        width: double.infinity,
+        child: Shimmer.fromColors(
+          baseColor: colors.surface,
+          highlightColor: colors.surfaceLow,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
             ),
-          )
-        ]
-    );
+          ),
+        ),
+      )
+    ]);
   }
 }
