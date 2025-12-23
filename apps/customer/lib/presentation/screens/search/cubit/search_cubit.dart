cefccaf8021821f08127b20b8e09a1d0501af917
MuhammedWael9991 <mw26200400@gmail.com>
2025-12-23 17:16:59 +0200

    refactor: enhance SearchCubit to support multiple search types and improve recent searches handling

--- apps/customer/lib/presentation/screens/search/cubit/search_cubit.dart
+++ apps/customer/lib/presentation/screens/search/cubit/search_cubit.dart
@@ -1,51 +1,104 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
+import 'package:sellio_mobile/domain/entities/address.dart';
 import 'package:sellio_mobile/domain/entities/product.dart';
 import 'package:sellio_mobile/domain/repositories/search_repository.dart';
 import '../../../../core/error/result.dart';
+import '../../../../domain/entities/store.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
   final SearchRepository _searchRepository;
 
-  SearchCubit(this._searchRepository) : super(const SearchInitial());
+  SearchType _currentType = SearchType.products;
+  String _lastQuery = '';
+
+
+  SearchCubit(this._searchRepository)
+      : super(const SearchInitial(selectedType: SearchType.products));
 
   Future<void> init() async {
-    final recentSearches = await _searchRepository.getRecentSearches();
+    final result = await _searchRepository.getRecentSearches();
 
-    if (recentSearches.isSuccess) {
-      final recent = (recentSearches as Success<List<String>>).data;
-
-      if (recent.isEmpty) {
-        emit(const SearchInitial());
-      } else {
-        emit(SearchRecent(List.from(recent)));
-      }
+    if (result.isSuccess) {
+      emit(SearchRecent(
+        recentSearches: (result as Success<List<String>>).data,
+        selectedType: _currentType,
+      ));
     }
   }
 
-  Future<void> search(String text) async {
-    if (text.isEmpty) {
+  Future<void> search(String query) async {
+    _lastQuery = query;
+
+    if (query.isEmpty) {
       init();
       return;
     }
-    final result = await _searchRepository.searchProducts(query: text);
+
+    switch (_currentType) {
+      case SearchType.products:
+        await _searchProducts(query);
+        break;
+      case SearchType.stores:
+        await _searchStores(query);
+        break;
+    }
+  }
+
+  Future<void> _searchProducts(String query) async {
+    final result = await _searchRepository.searchProducts(query: query);
 
     if (result.isSuccess) {
       final products = (result as Success<List<Product>>).data;
-      if (products.isNotEmpty) {
-        await _searchRepository.addToRecentSearch(text);
-        emit(SearchSuccess(products));
-      } else {
-        emit(SearchEmpty());
-      }
+
+      emit(
+        products.isEmpty
+            ? SearchEmpty(selectedType: _currentType)
+            : SearchProductsSuccess(
+          products: products,
+          selectedType: _currentType,
+        ),
+      );
+    }
+  }
+
+  Future<void> _searchStores(String query) async {
+    final result = await _searchRepository.searchStores(query: query);
+
+    if (result.isSuccess) {
+      final stores = (result as Success<List<Store>>).data;
+      print("store: searchCubit result = $stores");
+
+      emit(
+        stores.isEmpty
+            ? SearchEmpty(selectedType: _currentType)
+            : SearchStoresSuccess(
+          stores: stores,
+          selectedType: _currentType,
+        ),
+      );
+    }
+  }
+
+  void selectTab(SearchType type) {
+
+    if (_currentType == type) return;
+    print("selectedTab: $type");
+
+    _currentType = type;
+
+    emit(SearchLoading(selectedType: _currentType));
+
+    if (_lastQuery.isNotEmpty) {
+    search(_lastQuery);
     }
   }
 
   void clearRecent() {
     _searchRepository.clearAllRecentSearches();
-    emit(const SearchInitial());
+    emit(SearchInitial(selectedType: _currentType));
   }
 
   void selectRecent(String text) {
