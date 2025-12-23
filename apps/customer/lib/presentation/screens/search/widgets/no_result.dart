1266bed64acd2b25bab90d7e92df91df3061d662
MuhammedWael9991 <mw26200400@gmail.com>
2025-12-23 22:44:53 +0200

    refactor: enhance no results UI in search screen with improved layout and styling

--- apps/customer/lib/presentation/screens/search/widgets/no_result.dart
+++ apps/customer/lib/presentation/screens/search/widgets/no_result.dart
@@ -1,4 +1,5 @@
 import 'package:design_system/constants/app_images.dart';
+import 'package:design_system/themes/sellio_theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gap/flutter_gap.dart';
 import 'package:sellio_mobile/presentation/screens/search/widgets/category_section.dart';
@@ -10,15 +11,44 @@ class NoResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CategorySection(),
-        const Gap(24),
-        Image.asset(AppImages.noResultSearchIcon),
-        Text(context.local.no_results_found),
-        Text(
-          context.local.please_check_your_spelling_or_try_a_different_search,
-        ),
-      ],
-    );  }
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: Column(
+        children: [
+          const CategorySection(),
+
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(AppImages.noResultSearchIcon),
+                  const Gap(16),
+
+                  Text(
+                    context.local.no_results_found,
+                    textAlign: TextAlign.center,
+                    style: context.theme.typography.textTheme.titleSmall.copyWith(
+                      color: context.theme.colors.title,
+                    ),
+                  ),
+
+                  const Gap(8),
+
+                  Text(
+                    context.local
+                        .please_check_your_spelling_or_try_a_different_search,
+                    textAlign: TextAlign.center,
+                    style: context.theme.typography.textTheme.bodySmall.copyWith(
+                      color: context.theme.colors.body,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
