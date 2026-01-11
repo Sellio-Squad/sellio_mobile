bf9a2db4879c19aa54afb0832a4051785388caa5
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-01-11 19:37:15 +0200

    feat: Integrate country_picker and enhance phone validation

--- apps/customer/lib/presentation/screens/account/account_settings/cubit/account_settings_state.dart
+++ apps/customer/lib/presentation/screens/account/account_settings/cubit/account_settings_state.dart
@@ -1,6 +1,5 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:equatable/equatable.dart';
-
-import '../country.dart';
 
 class AccountSettingsState extends Equatable {
   final String phoneNumber;
@@ -9,8 +8,10 @@ class AccountSettingsState extends Equatable {
   final bool isSuccess;
   final String? errorMessage;
   final bool isPhoneValid;
+  final Country? selectedCountry;
 
-  bool get isFormValid => phoneNumber.isNotEmpty && fullName.isNotEmpty && isPhoneValid;
+  bool get isFormValid =>
+      phoneNumber.isNotEmpty && fullName.isNotEmpty && isPhoneValid;
 
   const AccountSettingsState({
     this.phoneNumber = '',
@@ -19,17 +20,21 @@ class AccountSettingsState extends Equatable {
     this.isSuccess = false,
     this.errorMessage,
     this.isPhoneValid = false,
+    this.selectedCountry,
   });
 
   bool isPhoneNumberValid(Country country, String phone) {
-    if (country.code == '+20') {
+    if (country.countryCode == 'EG') {
       return RegExp(r'^(0?1\d{9})$').hasMatch(phone);
-    } else if (country.code == '+964') {
+    } else if (country.countryCode == 'PS') {
+      return RegExp(r'^(0?5\d{8})$').hasMatch(phone);
+    } else if (country.countryCode == 'IQ') {
       return RegExp(r'^(0?7\d{9,10})$').hasMatch(phone);
+    } else if (country.countryCode == 'SY') {
+      return RegExp(r'^(0?9\d{8})$').hasMatch(phone);
     }
-    return false;
+    return RegExp(r'^\d{8,15}$').hasMatch(phone);
   }
-
 
   AccountSettingsState copyWith({
     String? phoneNumber,
@@ -38,6 +43,7 @@ class AccountSettingsState extends Equatable {
     bool? isSuccess,
     String? errorMessage,
     bool? isPhoneValid,
+    Country? selectedCountry,
   }) {
     return AccountSettingsState(
       phoneNumber: phoneNumber ?? this.phoneNumber,
@@ -46,16 +52,18 @@ class AccountSettingsState extends Equatable {
       isSuccess: isSuccess ?? this.isSuccess,
       errorMessage: errorMessage,
       isPhoneValid: isPhoneValid ?? this.isPhoneValid,
+      selectedCountry: selectedCountry ?? this.selectedCountry,
     );
   }
 
   @override
   List<Object?> get props => [
-    phoneNumber,
-    fullName,
-    isLoading,
-    isSuccess,
-    errorMessage,
-    isPhoneValid,
-  ];
+        phoneNumber,
+        fullName,
+        isLoading,
+        isSuccess,
+        errorMessage,
+        isPhoneValid,
+        selectedCountry,
+      ];
 }
