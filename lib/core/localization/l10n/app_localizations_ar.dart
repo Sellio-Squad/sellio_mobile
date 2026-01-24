8f0f8cfdba081726d442583a9331626ad96faf97
Baraa Abdulrahman <168970211+baraa0abd@users.noreply.github.com>
2026-01-24 17:29:17 +0300

    feat(l10n): Add initial English and Arabic localizations
    This commit introduces the foundation for internationalization (i18n) by adding localization files for English (`en`) and Arabic (`ar`).
    
    - **`app_localizations_en.dart`**: New file containing all English translation strings for the application.
    - **`app_localizations_ar.dart`**: New file containing all Arabic translation strings for the application.
    
    These files define key-value pairs for UI text, error messages, and other user-facing content, enabling the app to support both languages.

---
+++ lib/core/localization/l10n/app_localizations_ar.dart
@@ -0,0 +1,570 @@
+// ignore: unused_import
+// ignore_for_file: type=lint
+
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr implements AppLocalizations {
+  @override
+  String get guest => 'ضيف';
+
+  @override
+  String get should_not_be_empty => 'لا يجب أن يكون الحقل فارغاً';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forget_password => 'نسيت كلمة المرور؟';
+
+  @override
+  String get profile_photo_optional => 'صورة الملف الشخصي (اختياري)';
+
+  @override
+  String get upload => 'رفع';
+
+
+  @override
+  String get could_not_launch_email => "تعذر فتح البريد الإلكتروني";
+
+  @override
+  String get could_not_launch_phone => "تعذر الاتصال بالرقم";
+
+  @override
+  String get could_not_launch_whatsapp => "تعذر فتح واتساب";
+
+  @override
+  String get could_not_launch_facebook => "تعذر فتح فيسبوك";
+
+  @override
+  String get could_not_launch_website => "تعذر فتح موقع الويب";
+
+  @override
+  String get store_location => "موقع المتجر";
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get cart => 'عربة التسوق';
+
+  @override
+  String get title_login => 'مرحباً بعودتك!';
+
+  @override
+  String get subtitle_login => 'أدخل معلوماتك لتسجيل الدخول';
+
+  @override
+  String get phone_number => 'رقم الهاتف';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get create_account => 'انشاء حساب';
+
+  @override
+  String get continue_as_guest => 'المتابعة كزائر';
+
+  @override
+  String get title_par_forget_password => 'نسيت كلمة المرور؟';
+
+  @override
+  String get title_forget_password => 'نسيت كلمة المرور؟';
+
+  @override
+  String get subtitle_forget_password =>
+      'سنرسل رمز مكون من 4 أرقام إلى رقم هاتفك أدناه.';
+
+  @override
+  String get send => 'إرسال';
+
+  @override
+  String get enter_code => 'أدخل الرمز';
+
+  @override
+  String get set_new_password => 'تعيين كلمة مرور جديدة';
+
+  @override
+  String get subtitle_set_new_password =>
+      'يرجى إدخال كلمة المرور الجديدة الخاصة بك والتأكد من تذكرها في المرة القادمة';
+
+  @override
+  String get enter_your_information_to_create_account =>
+      'أدخل معلوماتك لإنشاء حساب';
+
+  @override
+  String get country => 'الدولة';
+
+  @override
+  String get city => 'المدينة';
+
+  @override
+  String get profile_photo => 'صورة الملف الشخصي (اختياري)';
+
+  @override
+  String get already_have_account => 'هل لديك حساب بالفعل؟';
+
+  @override
+  String get search => 'بحث';
+
+  @override
+  String get order_history => 'سجل الطلبات';
+
+  @override
+  String get add_more_items => 'إضافة المزيد من العناصر';
+
+  @override
+  String get note_about_order => 'ملاحظة حول الطلب';
+
+  @override
+  String get write_here => 'اكتب هنا';
+
+  @override
+  String get confirm_order => 'تأكيد الطلب';
+
+  @override
+  String get total_price => 'السعر الإجمالي';
+
+  @override
+  String get select => 'اختر';
+
+  @override
+  String get back_to_shopping => 'العودة للتسوق';
+
+  @override
+  String get order_received => 'تم استلام طلبك بنجاح.';
+
+  @override
+  String get current_password => 'كلمة المرور الحالية';
+
+  @override
+  String get new_password => 'كلمة المرور الجديدة';
+
+  @override
+  String get confirm_new_password => 'تأكيد كلمة المرور الجديدة';
+
+  @override
+  String get save_changes => 'حفظ التغييرات';
+
+  @override
+  String get notification => 'الإشعارات';
+
+  @override
+  String get empty_cart => 'عربة التسوق فارغة';
+
+  @override
+  String get store_details => 'تفاصيل المتجر';
+
+  @override
+  String get about_store => 'حول المتجر';
+
+  @override
+  String get contact_info => 'معلومات الاتصال';
+
+  @override
+  String get address => 'العنوان';
+
+  @override
+  String get email_title => 'فريقنا الودود هنا لمساعدتك';
+
+  @override
+  String get phone_title => '11:00 مساءً - 12:00 صباحاً';
+
+  @override
+  String get facebook_title => 'حسابنا على فيسبوك';
+
+  @override
+  String get whatsapp_title => 'اتصال واتساب';
+
+  @override
+  String get website_title => 'زيارة موقعنا الإلكتروني';
+
+  @override
+  String get email_subject => 'عميل Sellio';
+
+  @override
+  String get error_generic => 'حدث خطأ ما';
+
+  @override
+  String get error_loading_store => 'فشل في تحميل معلومات المتجر';
+
+  @override
+  String get error_no_internet => 'لا يوجد اتصال بالإنترنت';
+
+  @override
+  String get welcome => 'مرحباً';
+
+  @override
+  String get account => 'الحساب';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get account_settings => 'إعدادات الحساب';
+
+  @override
+  String get reset_password => 'إعادة تعيين كلمة المرور';
+
+  @override
+  String get app_version => 'إصدار التطبيق';
+
+  @override
+  String get top_stores => 'أفضل المتاجر';
+
+  @override
+  String get my_orders => 'طلباتي';
+
+  @override
+  String get my_favourites => 'المفضلة لدي';
+
+  @override
+  String get trending_products => 'المنتجات الرائجة';
+
+  @override
+  String get search_results => 'نتائج البحث';
+
+  @override
+  String get search_your_favorite_items => 'ابحث عن العناصر المفضلة لديك...';
+
+  @override
+  String get special_offers => 'عروض خاصة';
+
+  @override
+  String get add_to_cart => 'أضف إلى السلة';
+
+  @override
+  String get empty_cart_title => 'سلة التسوق الخاصة بك فارغة!';
+
+  @override
+  String get empty_cart_desc => 'ابدأ في استكشاف وشراء العناصر المفضلة لديك';
+
+  @override
+  String get empty_cart_button => 'ابدأ التصفح';
+
+  @override
+  String get reset => 'إعادة تعيين';
+
+  @override
+  String get note_optional => 'ملاحظة (اختياري)';
+
+  @override
+  String get all => 'الكل';
+
+  @override
+  String get food => 'طعام';
+
+  @override
+  String get drinks => 'مشروبات';
+
+  @override
+  String get clothes => 'ملابس';
+
+  @override
+  String get thrift => 'منتجات مستعملة';
+
+  @override
+  String get furniture => 'أثاث';
+
+  @override
+  String get books => 'كتب';
+
+  @override
+  String get electronics => 'إلكترونيات';
+
+  @override
+  String get account_screen => 'شاشة الحساب';
+
+  @override
+  String get no_products_available => 'لا توجد منتجات متاحة';
+
+  @override
+  String get featured_items => 'عنوانات مميزة';
+
+  @override
+  String get has_been_placed_successfully => 'تم الطلب بنجاح';
+
+  @override
+  String get has_been_delivered_successfully => 'تم التوصيل بنجاح';
+
+  @override
+  String get has_been_cancelled => 'تم الإلغاء';
+
+  @override
+  String get today => 'اليوم';
+
+  @override
+  String get yesterday => 'أمس';
+
+  @override
+  String get notifications => 'الإشعارات';
+
+  @override
+  String get notifications_will_appear_here =>
+      'عندما يكون لديك إشعارات، ستظهر هنا';
+
+  @override
+  String get no_notifications_yet => 'لا توجد إشعارات بعد';
+
+  @override
+  String get notification_dismissed => 'تم رفض الإشعار';
+
+  @override
+  String get error_loading_notifications => 'خطأ في تحميل الإشعارات';
+
+  @override
+  String get s => 'S';
+
+  @override
+  String get m => 'M';
+
+  @override
+  String get l => 'L';
+
+  @override
+  String get xl => 'XL';
+
+  @override
+  String get two_xl => '2XL';
+
+  @override
+  String get three_xl => '3XL';
+
+  @override
+  String get accessories_and_gifts => 'إكسسوارات وهدايا';
+
+  @override
+  String get home_and_decore => 'منزل وديكور';
+
+  @override
+  String get tech_accessories => 'إكسسوارات تقنية';
+
+  @override
+  String get business_and_branding => 'أعمال وعلامات تجارية';
+
+  @override
+  String get customize_your_product => 'خصص منتجك';
+
+  @override
+  String get choose_a_product_to_customize_it => 'اختر منتجًا لتخصيصه';
+
+  @override
+  String get upload_logo_or_image => 'رفع شعار أو صورة';
+
+  @override
+  String get size => 'المقاس';
+
+  @override
+  String get color => 'اللون';
+
+  @override
+  String get change_language => 'تغيير اللغة';
+
+  @override
+  String get english => 'الإنجليزية';
+
+  @override
+  String get arabic => 'العربية';
+
+  @override
+  String get save => 'حفظ';
+
+  @override
+  String get delete_account => 'حذف الحساب';
+
+  @override
+  String get account_options => 'خيارات الحساب';
+
+  @override
+  String get are_you_sure_to_continue_deleting_account =>
+      'هل أنت متأكد من المتابعة لحذف الحساب؟';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get are_you_sure_to_continue_logout =>
+      'هل أنت متأكد من المتابعة لتسجيل الخروج؟';
+
+  @override
+  String get start_exploring_and_saving_your_favorite_items_here =>
+      'ابدأ الاستكشاف وحفظ العناصر المفضلة لديك هنا.';
+
+  @override
+  String get no_favourite_items => 'لا توجد عنوانات مفضلة!';
+
+  @override
+  String get favorites => 'المفضلة';
+
+  @override
+  String get products => 'المنتجات';
+
+  @override
+  String get stores => 'المتاجر';
+
+  @override
+  String get recent_searches => 'عمليات البحث الحديثة';
+
+  @override
+  String get clear_all => 'مسح الكل';
+
+  @override
+  String get no_results_found => 'لم يتم العثور على نتائج!';
+
+  @override
+  String get please_check_your_spelling_or_try_a_different_search =>
+      'يرجى التحقق من تهجئة الكلمة أو تجربة بحث مختلف.';
+
+  @override
+  String get start_exploring_your_favorite_items =>
+      'ابدأ استكشاف العناصر المفضلة لديك!';
+
+  @override
+  String get iraq => 'العراق';
+
+  @override
+  String get egypt => 'مصر';
+
+  @override
+  String get processing => 'قيد المعالجة';
+
+  @override
+  String get completed => 'مكتمل';
+
+  @override
+  String get cancelled => 'ملغى';
+
+  @override
+  String get order => 'طلب';
+
+  @override
+  String get view_details => 'عرض التفاصيل';
+
+  @override
+  String get cancel_order => 'إلغاء الطلب';
+
+  @override
+  String get placed_on => 'تم الطلب في';
+
+  @override
+  String get order_again => 'اطلب مرة أخرى';
+
+  @override
+  String get items => 'عناصر';
+
+  @override
+  String get full_name => 'الاسم الكامل';
+
+  @override
+  String get confirm_password => 'تأكيد كلمة المرور';
+
+  @override
+  String get phone_number_must_be_at_least_10_digits =>
+      'يجب أن يكون رقم الهاتف 10 أرقام على الأقل';
+
+  @override
+  String get phone_number_digits_only =>
+      'يجب أن يحتوي رقم الهاتف على أرقام فقط';
+
+  @override
+  String get full_name_at_least_2_characters =>
+      'يجب أن يكون الاسم الكامل حرفين على الأقل';
+
+  @override
+  String get full_name_letters_only =>
+      'يجب أن يحتوي الاسم الكامل على أحرف ومسافات فقط';
+
+  @override
+  String get country_at_least_2_characters =>
+      'يجب أن يكون اسم الدولة حرفين على الأقل';
+
+  @override
+  String get country_letters_only =>
+      'يجب أن يحتوي اسم الدولة على أحرف ومسافات فقط';
+
+  @override
+  String get city_at_least_2_characters =>
+      'يجب أن يكون اسم المدينة حرفين على الأقل';
+
+  @override
+  String get city_letters_only =>
+      'يجب أن يحتوي اسم المدينة على أحرف ومسافات فقط';
+
+  @override
+  String get password_min_6_characters =>
+      'يجب أن تكون كلمة المرور 6 أحرف على الأقل';
+
+  @override
+  String get password_max_20_characters =>
+      'يجب أن تكون كلمة المرور أقل من 20 حرفًا';
+
+  @override
+  String get passwords_do_not_match => 'كلمتا المرور غير متطابقتين';
+
+  @override
+  String get failed_to_create_account =>
+      'فشل إنشاء الحساب. يرجى المحاولة مرة أخرى.';
+  
+
+  @override
+  String get enter_the_4digit =>
+      'يرجى إدخال الرمز المكون من 4 أرقام المرسل إلى رقم هاتفك.';
+
+  @override
+  String enter_the_4_digit_sent_to(Object phone_number) {
+    return 'يرجى إدخال الرمز المكون من 4 أرقام المرسل إلى $phone_number.';
+  }
+
+  @override
+  String get no_order_history => 'لا يوجد سجل طلبات!';
+
+  @override
+  String get start_exploring => 'ابدأ الاستكشاف';
+
+  @override
+  String get start_exploring_favorite_items =>
+      'ابدأ استكشاف وشراء العناصر المفضلة لديك';
+
+  @override
+  String total_reviews(Object totalReviews) {
+    return '($totalReviews تقييم)';
+  }
+
+  @override
+  String no_products_found_for(Object search_query) {
+    return 'لا توجد منتجات لـ $search_query';
+  }
+
+  @override
+  String your_order_from(Object orderId) {
+    return 'طلبك $orderId من ';
+  }
+
+  @override
+  String re_send_in_resend_countdown_Sec(Object resendCountdown) {
+    return 'إعادة الإرسال خلال $resendCountdown ثانية';
+  }
+
+  @override
+  String get dont_received_code => 'لم تستلم الرمز؟';
+
+  @override
+  String get re_send => 'إعادة الإرسال';
+
+  @override
+  String get login_successful => 'تم تسجيل الدخول بنجاح!';
+
+  @override
+  String get login_failed => 'فشل تسجيل الدخول. يرجى المحاولة مرة أخرى.';
+
+  @override
+  String get default_country_code => '+964';
+
+  @override
+  // TODO: implement locale
+  String get locale => throw UnimplementedError();
+}
