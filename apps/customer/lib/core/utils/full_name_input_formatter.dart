d045ede1cb7507093f53ef61e7fb54ec0bfe29dd
Israa Mohamed <e.mohamed@shabek.com>
2026-02-07 20:00:26 +0200

    refactor: improve full name validation

--- apps/customer/lib/core/utils/full_name_input_formatter.dart
+++ apps/customer/lib/core/utils/full_name_input_formatter.dart
@@ -2,21 +2,25 @@ import 'package:flutter/services.dart';
 
 class FullNameInputFormatter extends TextInputFormatter {
   static const int maxLength = 50;
-  static const String _allowedPattern = r'[a-zA-Z\u0600-\u06FF ]';
+
+  static final RegExp _allowedPattern = RegExp(r'^[a-zA-Z\u0600-\u06FF ]*$');
+  static final RegExp _doubleSpacePattern = RegExp(r' {2}');
 
   @override
   TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
+      TextEditingValue oldValue,
+      TextEditingValue newValue,
+      ) {
     if (newValue.text.startsWith(' ')) return oldValue;
 
-    if (newValue.text.contains('  ')) return oldValue;
+    if (_doubleSpacePattern.hasMatch(newValue.text)) return oldValue;
 
     if (newValue.text.length > maxLength) return oldValue;
-    if (!RegExp('^$_allowedPattern*\$').hasMatch(newValue.text)) {
+
+    if (!_allowedPattern.hasMatch(newValue.text)) {
       return oldValue;
     }
+
     return newValue;
   }
 }
