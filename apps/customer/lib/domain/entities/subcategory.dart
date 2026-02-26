b69da2c36be05435d5d5fbd785ea8c62b21287e0
Mohamed <m.3.abdelfadeel@gmail.com>
2026-02-26 17:19:03 +0200

    feat(category-details): Add remote data source and repository for category details

---
+++ apps/customer/lib/domain/entities/subcategory.dart
@@ -0,0 +1,11 @@
+class Subcategory {
+  final String id;
+  final String name;
+  final String? imageUrl;
+
+  const Subcategory({
+    required this.id,
+    required this.name,
+    this.imageUrl,
+  });
+}
