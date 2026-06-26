fed4af9fdb0b432ecf398b49a2f42862d5d7be33
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-06-26 23:33:33 +0300

    feat: add NavigationModule and register navigators
    - Create `NavigationModule` to manage dependency injection for navigation.
    - Register `AppNavigator` and `AuthNavigator` as lazy singletons using `GetIt`.

---
+++ apps/seller/lib/di/modules/navigation_module.dart
@@ -0,0 +1,11 @@
+import 'package:authentication/authentication.dart';
+import 'package:get_it/get_it.dart';
+import '../../core/navigate/app_navigator.dart';
+import '../../core/navigate/app_navigator_impl.dart';
+
+class NavigationModule {
+  static void register(GetIt sl) {
+    sl.registerLazySingleton<AppNavigator>(() => AppNavigatorImpl());
+    sl.registerLazySingleton<AuthNavigator>(() => sl<AppNavigator>());
+  }
+}
